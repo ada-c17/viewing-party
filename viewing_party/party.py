43c6c75f27a0cd623d1c810566363dd613e5e671
Victoria Shade <victoria.k.shade@gmail.com>
2022-03-17 14:58:38 -0700

    wave 1 in progress

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,40 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
-    "git test 1, 2, 3"
+    movie = {}
+    
+    movie["title"] = "Title A"
+    movie["genre"] = "Horror"
+    movie["rating"] = 3.5
+
+    if (movie["title"]) == "" or (movie["genre"]) == "" or (movie["rating"]) == "":
+        return None
+
+
+def add_to_watched(user_data, movie):
+    
+    watched = []
+    user_data = {watched: movie}
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    watchlist = []
+    user_data = {watchlist, watched}
+    return user_data
+
+#the value of user_data will be a dictionary with a key "watched", and a value which is a list of dictionaries representing the movies the user has watched
+#An empty list represents that the user has no movies in their watched list
+#the value of movie will be a dictionary in this format:
+
+
+
+
+#If those three attributes are truthy, then return a dictionary. This dictionary should...
+# Have three key-value pairs, with specific keys
+# The three keys should be "title", "genre", and "rating"
+# The values of these key-value pairs should be appropriate values
+# If title is falsy, genre is falsy, or rating is falsy, this function should return None
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
