449de90c88a5bf199a5ed6ae6cabdf5136ce2481
Alexa Coffman <coffman.alexa@gmail.com>
2022-03-17 14:57:21 -0700

    updates party.py to pass tests 1-7 in wave test_wave_01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,12 +1,30 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title == None or genre == None or rating == None:
+        movies = None
+    else:
+        movies = {
+            "title" : title,
+            "genre" : genre,
+            "rating" : rating
+        }
+    return movies
 
-# -----------------------------------------
-# ------------- WAVE 2 --------------------
-# -----------------------------------------
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
 
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, movie):
+    for watched_movie in user_data["watchlist"]:
+        if watched_movie["title"] == movie:
+            user_data["watchlist"].remove(watched_movie)
+            user_data["watched"].append(watched_movie)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
