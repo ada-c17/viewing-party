509efd6986025786b1b65508304eb43aed6b149f
Natascha <njenkns@gmail.com>
2022-03-17 15:24:09 -0700

    added new movie dictionary

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -3,7 +3,12 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    new_movie = {
+        "title": title,
+        "genre": genre,
+        "rating": rating
+    }
+    return new_movie
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
