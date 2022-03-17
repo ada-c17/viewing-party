461f3b656cf0724099c1aad21efad4b298434a21
Nikki Torab <nikkitorab@gmail.com>
2022-03-17 14:36:36 -0700

    create_movie() & add_to_watched()

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,11 +1,22 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    if title == True and genre == True and rating == True:
-        ...
+    #if title == True and genre == True and rating == True:
+    #if isinstance(title, str) and isinstance(genre, str) and isinstance(rating, str):   
+    if title != None and genre != None and rating != None:
         movie_dict = {"title":title, "genre":genre, "rating":rating}
         return movie_dict
-    return None
+    else: 
+        return None
+
+
+
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+
 
 #hi
 # -----------------------------------------
