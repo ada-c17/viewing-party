7c48fec85cc3c5a3380ff28ba0abf55733abc305
Kiana <kianashui@gmail.com>
2022-03-17 14:47:59 -0700

    added add to watch function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -11,6 +11,18 @@ def create_movie(title, genre, rating):
     
     return movie
 
+def add_to_watched(data, movie):
+    data["watched"].append(movie)
+    return data
+
+movie = create_movie("title 1", "genre 1", "rating 1")
+
+user_data = {
+        "watched": []
+    }
+
+updated_data = add_to_watched(user_data, movie)
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
