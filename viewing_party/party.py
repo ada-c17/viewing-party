f6347a9958cbc187135d7b5acadf7b2473bb29a2
Ria Pastor <ria.jacqueline.pastor@gmail.com>
2022-03-17 17:27:16 -0400

    Wave 1 tests passing.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,27 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if not (title and genre and rating):
+        return None
+    return {"title":title, "genre":genre,"rating":rating}
 
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, movie_title):
+    updated_watchlist = []
+    for movie in user_data["watchlist"]:
+        if movie_title != movie["title"]:
+            updated_watchlist.append(movie)
+        else:
+            user_data["watched"].append(movie)
+    user_data["watchlist"] = updated_watchlist
+    return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
