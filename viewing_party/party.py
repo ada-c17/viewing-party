e4787ae80db9daf4f45c44011af592c3627a9e10
Megan Korling <korlingm@gmail.com>
2022-03-17 15:08:42 -0700

    created watch_movie, updated add_to_watched and watchlist to pass test

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -12,15 +12,33 @@ def create_movie(title, genre, rating):
     return movie_dict
 
 def add_to_watched(user_data, movie):
-    for value in user_data.values():
-        value = value.append(movie)
+    for key,value in user_data.items():
+        if key == "watched":
+            value.append(movie)
     return user_data
 
 def add_to_watchlist(user_data, movie):
-    for value in user_data.values():
-        value = value.append(movie)
+    for key,value in user_data.items():
+        if key == "watchlist":
+            value.append(movie)
     return user_data
 
+def watch_movie(user_data, title):
+    index = 0
+    transfer_movie = False
+    # dictionary
+    for key,value in user_data.items():
+        if key == "watchlist":
+            # list
+            for movie in value:
+                # dictionary
+                m_key = "title"
+                if movie[m_key] == title:
+                    transfer_movie = value.pop(index)
+                index += 1
+        if key == "watched" and transfer_movie:
+            user_data[key].append(transfer_movie)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
