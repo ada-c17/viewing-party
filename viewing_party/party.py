f42c4376c1a423aef0b7f0548cba306305006875
Roshni Patel <rpatel01@wesleyan.edu>
2022-03-17 14:10:45 -0700

    Work on create game, tests passing

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,24 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    # clarify why this doesn't work
+    # if title or genre or rating is None:
+    #     return None 
+        
+    new_movie = {
+        "title": title, 
+        "genre": genre,
+        "rating": rating 
+    }
+
+    if title == None or genre == None or rating == None:
+        new_movie = None 
+    return new_movie
+
+
+# def add_to_watched(user_data, movie):
+#     user_data["watchlist"].append(movie)
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
