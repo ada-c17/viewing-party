a2079bc22217a360d749b63cf1f4ca9aae06526a
Lulu Sun <sunrey32@uw.edu>
2022-03-17 16:38:07 -0700

    finished testing wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,38 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title == None or genre == None or rating == None:
+        return None
+    
+    movie = {}
+    movie['title'] = title
+    movie["genre"] = genre
+    movie["rating"] = rating
+
+    return movie
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+
+    return user_data
+
+def watch_movie(user_data, movie_title):
+    watched_movie = {}
+    #find movie_title in user_data["watchlist"]
+    for movie in user_data["watchlist"]:
+        # movie is a dict
+        if movie['title'] == movie_title:
+            # add movieto user_data["watched"]
+            user_data["watched"].append(movie)
+            # remove movie_title in user_data["watchlist"]
+            user_data["watchlist"].remove(movie)
+
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
