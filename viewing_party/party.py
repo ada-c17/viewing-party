3e7384ac2b6f223e50e5af9f2cc53caa8587c5e5
Ying G <yingg@yings-air.lan>
2022-03-17 16:59:18 -0700

    pass all wave 1 tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,23 +1,20 @@
-# ------------- WAVE 1 --------------------
+def create_movie(title,genre,rating):
+    if not bool(title) or not bool(genre) or not bool(rating):
+        return None
+    return {"title":title,"genre":genre,"rating":rating}
 
-def create_movie(title, genre, rating):
-    pass
+def add_to_watched(user_data,movie):
+    user_data["watched"].append(movie)
+    return user_data
 
-# -----------------------------------------
-# ------------- WAVE 2 --------------------
-# -----------------------------------------
+def add_to_watchlist(user_data,movie):
+    user_data["watchlist"].append(movie)
+    return user_data
 
-
-# -----------------------------------------
-# ------------- WAVE 3 --------------------
-# -----------------------------------------
-
-        
-# -----------------------------------------
-# ------------- WAVE 4 --------------------
-# -----------------------------------------
-
-# -----------------------------------------
-# ------------- WAVE 5 --------------------
-# -----------------------------------------
-
+def watch_movie(user_data,title):
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(movie)
+            return user_data
+    return user_data
