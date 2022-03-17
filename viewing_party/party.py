983bda4cce76101ece99205b8f3a5a04c8c7b12c
Adriana Gutierrez <jerez.adriana@gmail.com>
2022-03-17 17:39:16 -0400

    First 3 tests done

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -2,13 +2,20 @@
 
 def create_movie(title, genre, rating):
     new_movie = {}
-    if title == None:
+    if title == None or genre == None or rating == None:
         return None
     new_movie["title"] = title
     new_movie["genre"] = genre
     new_movie["rating"] = rating
     return new_movie
 
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
