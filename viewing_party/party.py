0b7326069055bf3e14c4084a97ee19045684cd76
Sophia Wagner <sophiewagnerla@gmail.com>
2022-03-17 14:59:18 -0700

    Passed first 4 tests of w1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,15 @@
 # ------------- WAVE 1 --------------------
 #this is where I will create a function
 def create_movie(title, genre, rating):
-    pass
+    if not title or not genre or not rating:
+        return None
+
+    movie_dictionary ={}
+    movie_dictionary["title"] = title
+    movie_dictionary["genre"] = genre
+    movie_dictionary["rating"] = rating
+
+    return movie_dictionary
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
