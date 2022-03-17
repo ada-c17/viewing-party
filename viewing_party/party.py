cde826e28702b2af32d72eccab0fa5fd8c60c347
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-03-17 16:03:50 -0700

    Added to functions watched and watchlist and all tests passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -10,7 +10,15 @@ def create_movie(title, genre, rating):
         new_movie["rating"] = rating
     return new_movie
 
+def add_to_watched(user_data, movie):
+    watched = user_data["watched"]
+    watched.append(movie)
+    return user_data
 
+def add_to_watchlist(user_data, movie):
+    watchlist = user_data["watchlist"]
+    watchlist.append(movie)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
