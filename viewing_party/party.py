952a733678051aa4ef465c8aed5f36b4bac28c1f
Uyen Vong <uvong92@gmail.com>
2022-03-17 14:43:53 -0700

    pass first 6 tests in wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,33 @@
 # ------------- WAVE 1 --------------------
 #adding a comment in the code to commit :)
 def create_movie(title, genre, rating):
-    pass
+    
+    user_data = {
+        "watched": [],
+        "watchlist": []
+        }
+
+    new_movie = {
+        "title": title,
+        "genre": genre,
+        "rating": rating
+    }
+    if None in new_movie.values():
+        return None
+
+    return new_movie
+
+def add_to_watched(user_data, movie):
+    user_data["watched"] += [movie]
+    return user_data
+    
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"] += [movie]
+    return user_data
+
+
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
