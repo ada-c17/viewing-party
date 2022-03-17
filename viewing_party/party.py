ae4a90621e3daf49e563a07b3e5e89284be29ded
Amel <amelntamark@gmail.com>
2022-03-17 16:25:38 -0700

    create_movie feature successfully returns dictionary

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -3,6 +3,11 @@
 def create_movie(title, genre, rating):
     if not title or not genre or not rating:
         return None
+    return {
+        "title": title,
+        "genre": genre,
+        "rating": rating
+    }
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
