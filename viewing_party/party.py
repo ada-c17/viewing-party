a7997a6f6697a1b9a91fa1d069603586556495e4
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-03-17 14:44:50 -0700

    Function create movie created and first four tests passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,16 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    new_movie = {}
+    if title == None or genre == None or rating == None:
+        return None
+    else:
+        new_movie["title"] = title
+        new_movie["genre"] = genre
+        new_movie["rating"] = rating
+    return new_movie
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
