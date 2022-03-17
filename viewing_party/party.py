fe9eb8e4ea675233dde88c149bd91279330a004c
Junnie <junniejlee@gmail.com>
2022-03-17 16:09:17 -0700

    Created all the function names

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,23 +1,52 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
+    movie_dict = {}
+    return movie_dict(title, genre, rating)
+    
+def add_to_watched(user_data, movie):
     pass
+
+def add_to_watchlist(user_data, movie):
+    pass
+
+def watch_movie(user_data, title):
+    pass
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    pass
+
+def get_most_watched_genre(user_data):
+    pass
+
 
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+def get_unique_watched(user_data):
+    pass
 
+def get_friends_unique_watched(user_data):
+    pass
         
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+def get_available_recs(user_data):
+    pass
+
+
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
+def get_new_rec_by_genre(user_data):
+    pass
 
+def get_rec_from_favorites(user_data):
+    pass
