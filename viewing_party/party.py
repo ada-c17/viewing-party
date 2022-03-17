0cc76443a0d0da58eb50bf872bab88d796fcff49
Emily Schell <lauraemilyschell@gmail.com>
2022-03-17 17:18:32 -0400

    initial commit pass all wave 1 tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,35 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    if title and genre and rating:
+        return {
+            "title": title,
+            "genre": genre,
+            "rating": rating
+        }
+    return None
+
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+
+def watch_movie(user_data, title):
+    updated_data = user_data.copy()
+
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            updated_data["watched"].append(movie)
+            updated_data["watchlist"].remove(movie)
+
+    return updated_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
@@ -12,7 +40,7 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-        
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
@@ -20,4 +48,3 @@ def create_movie(title, genre, rating):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
