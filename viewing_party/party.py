3e4002aeb3aacc4a34914df042c196768fe3c8f1
Morgan <morganheuer@gmail.com>
2022-03-17 15:18:21 -0700

    adds movie to user watched

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -10,6 +10,11 @@ def create_movie(title, genre, rating):
             return None
     return movie_dict
 
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
