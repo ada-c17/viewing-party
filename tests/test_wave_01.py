79179482196e653eea842dd7053a1b16c1b00624
Roshni Patel <rpatel01@wesleyan.edu>
2022-03-17 14:55:49 -0700

    Add assertions in tests and start working on other wave 1 functions

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -58,7 +58,7 @@ def test_create_no_rating_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_adds_movie_to_user_watched():
     # Arrange
     movie = {
@@ -79,7 +79,7 @@ def test_adds_movie_to_user_watched():
     assert updated_data["watched"][0]["genre"] is GENRE_1
     assert updated_data["watched"][0]["rating"] is RATING_1
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_adds_movie_to_user_watchlist():
     # Arrange
     movie = {
@@ -100,7 +100,7 @@ def test_adds_movie_to_user_watchlist():
     assert updated_data["watchlist"][0]["genre"] is GENRE_1
     assert updated_data["watchlist"][0]["rating"] is RATING_1
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_empty_watched():
     # Arrange
     janes_data = {
@@ -118,12 +118,14 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) is 0
     assert len(updated_data["watched"]) is 1
-    
+    assert MOVIE_TITLE_1 in updated_data["watched"] is True
+    assert MOVIE_TITLE_1 in updated_data["watchlist"] is False
+
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -141,12 +143,14 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) is 1
     assert len(updated_data["watched"]) is 2
-    
+    assert movie_to_watch in updated_data["watched"] is True
+    assert movie_to_watch in updated_data["watchlist"] is False
+
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -4,7 +4,7 @@ def create_movie(title, genre, rating):
     # clarify why this doesn't work
     # if title or genre or rating is None:
     #     return None 
-        
+
     new_movie = {
         "title": title, 
         "genre": genre,
@@ -16,10 +16,39 @@ def create_movie(title, genre, rating):
     return new_movie
 
 
-# def add_to_watched(user_data, movie):
-#     user_data["watchlist"].append(movie)
+def add_to_watched(user_data, movie):
+    # if movie:
+    #     return user_data["watched"].append(movie)
+    print(movie)
+    print(user_data)
+
+    updated_data = user_data["watched"].append(movie)
+    return updated_data
+    # if movie:
+    # # return user_data["watched"].append(movie)
+    #     return user_data["watched"].append({
+    #         "title": movie["title"],
+    #         "genre": movie["genre"],
+    #         "rating": movie["rating"]
+    #     })
+
+def add_to_watchlist(user_data, movie):
+    updated_watchlist = user_data["watchlist"].append(movie)
+    # updated_watchlist = user_data["watchlist"].append({
+    #         "title": movie["title"],
+    #         "genre": movie["genre"],
+    #         "rating": movie["rating"]
+    #     })
+
+    return updated_watchlist
 
 
+def watch_movie(user_data, title):
+    if title in user_data["watchlist"]:
+        user_data["watchlist"].remove(title)
+        user_data["watched"].append(title)
+    
+    return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
