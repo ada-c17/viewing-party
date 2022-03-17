ee790d7fcf5160e51251e5366b0f7119200cdafc
Natalya <chshebetun@gmail.com>
2022-03-17 14:55:50 -0700

    Made functions create movie, add to watched & watchlist

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,24 @@
 # ------------- WAVE 1 --------------------
 
+from imp import new_module
+
+
 def create_movie(title, genre, rating):
-    pass
+    new_movie = {}
+    if title == None or genre == None or rating == None:
+        return None
+    new_movie['title'] = title
+    new_movie['genre'] = genre
+    new_movie['rating'] = rating 
+    return new_movie
+
+def add_to_watched(user_data, movie):
+    user_data['watched'].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data['watchlist'].append(movie)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
