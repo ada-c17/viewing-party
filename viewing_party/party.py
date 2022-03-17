0cd7c364c352ce8029dd1dfd3cf9335c010f15d2
Adriana Gutierrez <jerez.adriana@gmail.com>
2022-03-17 19:03:08 -0400

    watch movie working before final test

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -16,6 +16,22 @@ def add_to_watched(user_data, movie):
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
+
+def watch_movie(user_data, title):
+    index = 0
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            move_movie = movie
+            break
+        index += 1
+    if index > len(user_data["watchlist"]):
+        return user_data
+    user_data["watched"].append(move_movie)
+    pop_movie = user_data["watchlist"][index]
+    user_data["watchlist"].pop(index)
+    return user_data
+
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
