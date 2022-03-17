605221d4a24559ff9f3948452969728922c35fb8
MandyG415 <missmandygao@gmail.com>
2022-03-17 16:21:39 -0700

    Created create_movie and add_to_watched function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,21 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+  dict = {}
+  if title and genre and rating:
+    return {
+      "title": title,
+      "genre": genre,
+      "rating": rating,
+    }
+  else:
+    return None
 
+def add_to_watched(user_data, movie):
+  # user_data = {"watched": [ {} {}]}
+  # empty [] means user has no movies 
+  user_data["watched"].append(movie) #append returns None
+  return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
