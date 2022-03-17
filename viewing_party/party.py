73a2cac46bf2c2c70d2f295c425d76cb1191cf62
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-03-17 16:07:59 -0500

    Pass test_wave_01 test_create_successful_movie()

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -2,6 +2,16 @@
 
 def create_movie(title, genre, rating):
     pass
+    # using the parameters title, genre, and rating, create a dictionary called new_movie
+    # with keys "title", "genre", and "rating"
+    # and values that are equal to title, genre, rating
+
+    new_movie = {}
+    new_movie["title"] = title
+    new_movie["genre"] = genre
+    new_movie["rating"] = rating
+
+    return new_movie
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
