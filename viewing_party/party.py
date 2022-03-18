ff4ccac2e3da581f4dc86a8993bfa73c2f31415b
Ruge Li <rugeli0605@gmail.com>
2022-03-18 15:44:07 -0700

    wave01-9-passed-completed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -13,9 +13,21 @@ def create_movie(title, genre, rating):
     return movie_dict
 
 def add_to_watched(user_data,movie):
-    
     user_data["watched"].append(movie)
     return user_data
+
+def add_to_watchlist(user_data,movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data,title):
+    for movie in user_data["watchlist"]:
+        if title == movie["title"]:
+            user_data["watched"].append(movie)
+            user_data["watchlist"].remove(movie)
+    return user_data
+       
+            
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
