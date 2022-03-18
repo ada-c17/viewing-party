60536192e7c48f3ac0a79632eda8f3b414dec3b0
Amel <amelntamark@gmail.com>
2022-03-18 11:44:45 -0700

    get_watched_avg_rating returns 0.0 if ratings average is 0

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -34,79 +34,12 @@ def get_watched_avg_rating(user_data):
     ratings_sum = 0.0
     for rating in ratings:
         ratings_sum += rating
-    avg_rating = ratings_sum / len(ratings)
+    
+    if ratings_sum != 0: # Calculates avg if ratings sum is not zero
+        avg_rating = ratings_sum / len(ratings)
+    else:
+        return 0.0
     return avg_rating
-
-
-
-# HORROR_1 = {
-#     "title": "Us",
-#     "genre": "Horror",
-#     "rating": 5.0
-# }
-# FANTASY_1 = {
-#     "title": "The Lord of the Functions: The Fellowship of the Function",
-#     "genre": "Fantasy",
-#     "rating": 4.8
-# }
-# FANTASY_2 = {
-#     "title": "The Lord of the Functions: The Two Parameters",
-#     "genre": "Fantasy",
-#     "rating": 4.0
-# }
-# FANTASY_3 = {
-#     "title": "The Lord of the Functions: The Return of the Value",
-#     "genre": "Fantasy",
-#     "rating": 4.0
-# }
-# FANTASY_4 = {
-#     "title": "The Programmer: An Unexpected Stack Trace",
-#     "genre": "Fantasy",
-#     "rating": 4.0
-# }
-# ACTION_1 = {
-#     "title": "The JavaScript and the React",
-#     "genre": "Action",
-#     "rating": 2.2
-# }
-# ACTION_2 = {
-#     "title": "2 JavaScript 2 React",
-#     "genre": "Action",
-#     "rating": 4.2
-# }
-# ACTION_3 = {
-#     "title": "JavaScript 3: VS Code Lint",
-#     "genre": "Action",
-#     "rating": 3.5
-# }
-# INTRIGUE_1 = {
-#     "title": "Recursion",
-#     "genre": "Intrigue",
-#     "rating": 2.0
-# }
-# INTRIGUE_2 = {
-#     "title": "Instructor Student TA Manager",
-#     "genre": "Intrigue",
-#     "rating": 4.5
-# }
-# INTRIGUE_3 = {
-#     "title": "Zero Dark Python",
-#     "genre": "Intrigue",
-#     "rating": 3.0
-# }
-
-# USER_DATA_2 = {
-#     "watched": [
-#         FANTASY_1, 
-#         FANTASY_2, 
-#         FANTASY_3, 
-#         ACTION_1, 
-#         INTRIGUE_1, 
-#         INTRIGUE_2
-#         ],    
-# }
-
-# get_watched_avg_rating(USER_DATA_2)
 
 
 # -----------------------------------------
