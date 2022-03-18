4db5ddb6cb234caa8e1ea4f48da797840b23852b
Olena <olenazachkow@gmail.com>
2022-03-18 12:08:04 -0700

    wave 1 tests passed

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -60,7 +60,7 @@ def test_create_no_rating_movie():
 #@pytest.mark.skip()
 def test_adds_movie_to_user_watched():
     # Arrange
-    movie = {
+    movie = { 
         "title": MOVIE_TITLE_1,
         "genre": GENRE_1,
         "rating": RATING_1
@@ -78,7 +78,7 @@ def test_adds_movie_to_user_watched():
     assert updated_data["watched"][0]["genre"] is GENRE_1
     assert updated_data["watched"][0]["rating"] is RATING_1
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_adds_movie_to_user_watchlist():
     # Arrange
     movie = {
@@ -99,7 +99,7 @@ def test_adds_movie_to_user_watchlist():
     assert updated_data["watchlist"][0]["genre"] is GENRE_1
     assert updated_data["watchlist"][0]["rating"] is RATING_1
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_empty_watched():
     # Arrange
     janes_data = {
@@ -118,11 +118,13 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert len(updated_data["watchlist"]) is 0
     assert len(updated_data["watched"]) is 1
     
+    assert updated_data["watched"][0]["title"] is MOVIE_TITLE_1
+
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -140,12 +142,19 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) is 1
     assert len(updated_data["watched"]) is 2
-    
+    watchedMovies = updated_data["watched"]
+    secondWatchedMovie = watchedMovies[1]
+    secondWatchedMovieTitle = secondWatchedMovie["title"]
+
+    horror1Title = HORROR_1["title"]
+
+    assert horror1Title is secondWatchedMovieTitle
+
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -16,7 +16,19 @@ def add_to_watched(user_data, movie):
     user_data["watched"].append(movie)
     return user_data
 
-    
+def  add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(janes_data, MOVIE_TITLE_1):
+    for movie in janes_data["watchlist"]:
+        if movie["title"]==MOVIE_TITLE_1:
+            watched_movie=movie
+            janes_data["watched"].append(watched_movie)
+            janes_data["watchlist"].remove(movie)
+            break
+    return (janes_data)
+        
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
