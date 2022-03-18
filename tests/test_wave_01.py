eddb0c9b31b6c5e3cf0f3f49fa93610dacda611a
Natalya <chshebetun@gmail.com>
2022-03-18 11:42:39 -0700

    All 9 tests passed

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -118,12 +118,12 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) is 0
     assert len(updated_data["watched"]) is 1
-    
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    assert updated_data["watched"][0]["title"] is MOVIE_TITLE_1
+    assert updated_data["watched"][0]["genre"] is GENRE_1
+    assert updated_data["watched"][0]["rating"] is RATING_1    
 
-@pytest.mark.skip()
+
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -141,12 +141,11 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) is 1
     assert len(updated_data["watched"]) is 2
-    
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    assert updated_data["watched"][1]["title"] is MOVIE_TITLE_1
+    assert updated_data["watched"][1]["genre"] is GENRE_1
+    assert updated_data["watched"][1]["rating"] is RATING_1  
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
