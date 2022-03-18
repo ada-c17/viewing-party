4913f2eafccd984db77401de43e5102731c14bb3
Megan Korling <korlingm@gmail.com>
2022-03-17 18:53:06 -0700

    added assert statement to 4th test and passed 5th test

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -54,12 +54,10 @@ def test_friends_unique_movies_not_duplicated():
 
     # Arrange
     assert len(friends_unique_movies) == 3
-
-    # *************************************************************************************************
     # ****** Add assertions here to test that the correct movies are in friends_unique_movies **********
-    # **************************************************************************************************
+    assert friends_unique_movies == [FANTASY_4, HORROR_1, INTRIGUE_3]
 
-@pytest.mark.skip()
+pytest.mark.skip()
 def test_friends_not_unique_movies():
     # Arrange
     amandas_data = {
