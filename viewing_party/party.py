b2f5a320c1ef44509b29630bd06999e9e7fb8277
Natascha <njenkns@gmail.com>
2022-03-17 17:34:28 -0700

    added conditional to check for none

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,14 +1,19 @@
-# just making a comment
+
 
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
+    # creating a dictionary that stores the variables passed into the function as the values
     new_movie = {
         "title": title,
         "genre": genre,
         "rating": rating
     }
-    return new_movie
+    # checks to make sure that the variables passed into the function are real values and not none. If none, returns none. If valid input, then returns the dict.
+    if title is None or genre is None or rating is None:
+        return None
+    else:
+        return new_movie
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
