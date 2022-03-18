fec77b6f6cbf5861b9b05d8608379e3919ae2212
Alexa Coffman <coffman.alexa@gmail.com>
2022-03-18 11:38:13 -0700

    Updated wave 1 test confirming movie correctly added to non-empty watched list

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -122,7 +122,7 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     assert updated_data["watched"][0]["genre"] is GENRE_1
     assert updated_data["watched"][0]["rating"] is RATING_1
 
-@pytest.mark.skip()
+
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -140,10 +140,7 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) is 1
     assert len(updated_data["watched"]) is 2
-    
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    assert updated_data["watched"][-1] is movie_to_watch
 
 @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
