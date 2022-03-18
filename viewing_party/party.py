d7d42e6e352ec806f067f38656a75f90ac5fdb49
verasazonova <vera.zhokhova@gmail.com>
2022-03-18 11:31:56 -0700

    test 1-6 passed,change requirments pytest version

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,34 @@
 # ------------- WAVE 1 --------------------
+import pytest
+from tests.test_constants import GENRE_1, MOVIE_TITLE_1, RATING_1
 
-def create_movie(title, genre, rating):
-    pass
 
+def create_movie(movie_title, genre, rating):    
+
+    if movie_title == None or genre == None or rating == None:
+        return None
+
+    new_movie = {
+        "title" : movie_title,
+        "genre" : genre,
+        "rating" : rating
+    }
+
+    return new_movie
+
+def add_to_watched(user_data, movie):
+
+    list = user_data["watched"]
+    list.append(movie)
+
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+
+    list = user_data["watchlist"]
+    list.append(movie)
+
+    return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
