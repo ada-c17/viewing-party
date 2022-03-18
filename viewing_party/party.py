40ec395f5e20d3fa543163b65f0a6d0852ed81a5
tinghj17 <tinghj17@gmail.com>
2022-03-17 17:12:16 -0700

    complete wave1 version1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,11 @@
 # ------------- WAVE 1 --------------------
 
+from re import T
+
+
 def create_movie(title, genre, rating):
     movie_dict = {}
-    if title or genre or rating:
+    if title and genre and rating:
         movie_dict["title"] = title
         movie_dict["genre"] = genre
         movie_dict["rating"] = rating
@@ -10,6 +13,21 @@ def create_movie(title, genre, rating):
     else:
         return None
 
+def add_to_watched(user_data, movie):
+    user_data["watched"] = [movie]
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"] = [movie]
+    return user_data
+
+def watch_movie(user_data, title):
+    for index in range(len(user_data["watchlist"])):
+        if title == user_data["watchlist"][index]["title"]:
+            watched_movie = user_data["watchlist"][index]
+            user_data["watchlist"].remove(watched_movie)
+            user_data["watched"].append(watched_movie)
+    return user_data
 
 
 # -----------------------------------------
