920080dcdc19f00c96f48a9ed6b6944e6b34a82a
Fena Dang <fenadang@gmail.com>
2022-03-18 11:22:03 -0700

    completed wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -19,12 +19,13 @@ def add_to_watchlist(user_data, movie):
 
 def watch_movie(user_data, title):
     for i in range(len(user_data["watchlist"])):
+        print(f"i:{i}")
+        print(f"value:{user_data['watchlist'][i]['title']}")
         if user_data["watchlist"][i]["title"] == title:
-            user_data["watched"] = [user_data["watchlist"][i]]
+            user_data["watched"].append(user_data["watchlist"][i])
             user_data["watchlist"].pop(i)
             return user_data
-        else:
-            return user_data
+    return user_data
 
 
 # janes_data = {
@@ -32,10 +33,15 @@ def watch_movie(user_data, title):
 #         "title": "a",
 #         "genre": "b",
 #         "rating": "c"
+#     }, 
+#     {
+#         "title": "d",
+#         "genre": "e",
+#         "rating": "f"
 #     }],
-#     "watched": []
+#     "watched": ["p"]
 # }
-# print(watch_movie(janes_data, "a"))
+# print(watch_movie(janes_data, "d"))
 
 
 
