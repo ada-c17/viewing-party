2e03ff90e4a1d644e7268cf6293fa67816feb881
Roshni Patel <rpatel01@wesleyan.edu>
2022-03-18 11:37:45 -0700

    Add to watched and add to watchlist passing

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,10 +1,6 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    # clarify why this doesn't work
-    # if title or genre or rating is None:
-    #     return None 
-
     new_movie = {
         "title": title, 
         "genre": genre,
@@ -17,38 +13,22 @@ def create_movie(title, genre, rating):
 
 
 def add_to_watched(user_data, movie):
-    # if movie:
-    #     return user_data["watched"].append(movie)
-    print(movie)
-    print(user_data)
+    user_data["watched"].append(movie)
+    return user_data
 
-    updated_data = user_data["watched"].append(movie)
-    return updated_data
-    # if movie:
-    # # return user_data["watched"].append(movie)
-    #     return user_data["watched"].append({
-    #         "title": movie["title"],
-    #         "genre": movie["genre"],
-    #         "rating": movie["rating"]
-    #     })
 
 def add_to_watchlist(user_data, movie):
-    updated_watchlist = user_data["watchlist"].append(movie)
-    # updated_watchlist = user_data["watchlist"].append({
-    #         "title": movie["title"],
-    #         "genre": movie["genre"],
-    #         "rating": movie["rating"]
-    #     })
-
-    return updated_watchlist
+    user_data["watchlist"].append(movie)
+    return user_data
 
 
 def watch_movie(user_data, title):
     if title in user_data["watchlist"]:
         user_data["watchlist"].remove(title)
         user_data["watched"].append(title)
-    
     return user_data
+
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
