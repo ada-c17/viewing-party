ec4cf56ed684876c6288061ad0b3ebeeeabd9ec8
Amel <amelntamark@gmail.com>
2022-03-18 11:56:55 -0700

    created get_most_watched_genre. successfully gets most watched genre

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,3 +1,6 @@
+import statistics
+from statistics import mode
+
 # ------------- WAVE 1 --------------------
 def create_movie(title, genre, rating):
     if not title or not genre or not rating:
@@ -40,6 +43,16 @@ def get_watched_avg_rating(user_data):
     else:
         return 0.0
     return avg_rating
+
+def get_most_watched_genre(user_data):
+    genres = [] # List will hold every instance of a genre in user watched list
+    watched_list = user_data["watched"] # Extract watched list from rest of user data
+
+    for movie in watched_list:
+        genres.append(movie["genre"])
+
+    return mode(genres)
+    
 
 
 # -----------------------------------------
