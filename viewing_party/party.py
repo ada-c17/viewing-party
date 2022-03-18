0a5d2874cead72286895a45fb536ce66c0695ee4
Megan Korling <korlingm@gmail.com>
2022-03-18 11:31:20 -0700

    refactored watch_movie, all tests pass

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -27,19 +27,17 @@ def add_to_watchlist(user_data, movie):
 def watch_movie(user_data, title):
     index = 0
     transfer_movie = False
-    # dictionary
-    for key,value in user_data.items():
-        if key == "watchlist":
-            # list
-            for movie in value:
-                # dictionary
-                m_key = "title"
-                if movie[m_key] == title:
-                    transfer_movie = value.pop(index)
-                index += 1
-        if key == "watched" and transfer_movie:
-            user_data[key].append(transfer_movie)
+    watched_list = user_data["watched"]
+    watchlist = user_data["watchlist"]
+    for movie in watchlist:
+        if movie["title"] == title:
+            transfer_movie = watchlist.pop(index)
+            break
+        index += 1
+    if transfer_movie:
+        watched_list.append(transfer_movie)
     return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
