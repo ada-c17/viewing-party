ce6df48ff70fae107ff195bdde1d9b167aae4368
MandyG415 <missmandygao@gmail.com>
2022-03-18 12:32:06 -0700

    Finished with Wave 1, need to incorporate test 7 with index

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -17,12 +17,19 @@ def add_to_watched(user_data, movie):
   user_data["watched"].append(movie) #append returns None when assigned to a variable
   return user_data
 
-def add_to_watchlist(user_data, movie):
-  user_data["watchlist"].append(movie)
+def add_to_watchlist(user_data, movie): #adding movides to watchlist
+  user_data["watchlist"].append(movie) #via appending movie to dict
   return user_data
 
-#def watch_movie(user_data, title):
-#  if title in 
+def watch_movie(user_data, title):
+  if title == user_data["watchlist"][0]["title"]: #suggestion to make variables
+    #position = user_data["watchlist"].index("title")
+    user_data["watchlist"].pop(0) #leads to empty watchlist 
+    user_data["watched"].append(title)
+    return user_data
+  else:
+    return user_data
+  print (user_data)
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
