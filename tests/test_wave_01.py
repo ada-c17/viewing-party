f11df18a7aed3992c51f4588aa2dd7d616b735cd
NinaPatrina <ninochca@gmail.com>
2022-03-17 21:42:36 -0700

    Added add_to_watched function. Added add_to_watchlist function. Added watch_movie function. All tests passed in wave 1

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -59,7 +59,7 @@ def test_create_no_rating_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_adds_movie_to_user_watched():
     # Arrange
     movie = {
@@ -80,7 +80,7 @@ def test_adds_movie_to_user_watched():
     assert updated_data["watched"][0]["genre"] is GENRE_1
     assert updated_data["watched"][0]["rating"] is RATING_1
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_adds_movie_to_user_watchlist():
     # Arrange
     movie = {
@@ -101,7 +101,7 @@ def test_adds_movie_to_user_watchlist():
     assert updated_data["watchlist"][0]["genre"] is GENRE_1
     assert updated_data["watchlist"][0]["rating"] is RATING_1
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_empty_watched():
     # Arrange
     janes_data = {
@@ -124,7 +124,7 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -147,7 +147,7 @@ def test_moves_movie_from_watchlist_to_watched():
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,6 +1,4 @@
 # ------------- WAVE 1 -----------
-
-
 def create_movie(title, genre, rating):
     new_movie={}
     if title == None or genre == None or rating == None:
@@ -8,7 +6,24 @@ def create_movie(title, genre, rating):
     new_movie["title"]=title
     new_movie["genre"]=genre
     new_movie["rating"]=rating
+
     return new_movie
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, movie_title):
+    for data in user_data["watchlist"]:
+        if data["title"]== movie_title:
+            user_data["watchlist"].remove(data)
+            user_data["watched"].append(data)
+            return user_data 
+    return user_data    
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
