dd37277bee0392b73518119e2e282e0c683d09d0
Ying G <yingg@yings-air.lan>
2022-03-17 18:30:45 -0700

    pass all wave 2 tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -18,3 +18,26 @@ def watch_movie(user_data,title):
             user_data["watched"].append(movie)
             return user_data
     return user_data
+
+def get_watched_avg_rating(user_data):
+    ratings_sum = 0 
+    n = len(user_data["watched"])
+    if n == 0:
+        return 0
+    for movie in user_data["watched"]:
+        ratings_sum+=movie["rating"]
+    return ratings_sum/n
+        
+def get_most_watched_genre(user_data):
+    genre_counts = {}
+    if len(user_data["watched"]) == 0:
+        return None
+    for movie in user_data["watched"]:
+        genre = movie["genre"]
+        if genre in genre_counts:
+            genre_counts[movie["genre"]]+=1
+        else:
+            genre_counts[movie["genre"]] = 1
+    
+    key = max(genre_counts,key=lambda k: genre_counts[k])
+    return key
