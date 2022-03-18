7f93efa2c7a82182b56658429b4c4318ac070901
Lin Liu <lin_6066@yahoo.com>
2022-03-17 21:06:04 -0700

    completed wave_03

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -71,7 +71,48 @@ def get_most_watched_genre(user_data):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-        
+def get_unique_watched(user_data):
+
+    user_watched = user_data["watched"]                                     # A list of movies that user watched                      
+    friends_watched = get_friends_watched_movies(user_data)                 # A list of movies that friends watched                  
+    user_unique_watched = ele_in_a_not_b(user_watched, friends_watched)     # A list of movies that ONLY user watched
+
+    return user_unique_watched          
+
+def get_friends_unique_watched(user_data):
+
+    user_watched = user_data["watched"]                                     # A list of movies that user watched                      
+    friends_watched = get_friends_watched_movies(user_data)                 # A list of movies that friends watched 
+    friends_unique_watched = ele_in_a_not_b(friends_watched, user_watched)
+
+    return friends_unique_watched 
+
+#                                               *********** WAVE 3 Helper Functions **********
+def ele_in_a_not_b (a_lst, b_lst):
+    '''
+    Parameters: two lists
+    Return: a single list that contains elements in a_list but not in b_list 
+            the return list has no duplications
+    '''
+    output = []
+    for ele in a_lst:
+        if ele not in b_lst and ele not in output:                          # remove output duplications
+            output.append(ele)
+    return output
+
+def get_friends_watched_movies(user_data):
+    '''
+    Parameter: one user_data
+    Return: a single list that contains all friends watched movies
+    '''
+    friends_watched = []               
+
+    for friend in user_data["friends"]:         # user_data["friends"] is a list of dict,  dict key: "watched" - value: a list of watched movies 
+        for movie in friend["watched"]:
+                friends_watched.append(movie)
+
+    return friends_watched
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
