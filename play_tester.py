1392ef904c840876e6a406faabe56501225c7326
Chinazo Onwukaike <chinazoonwukaike@gmail.com>
2022-03-18 13:46:35 -0500

    Complete waves 1 and 2

--- play_tester.py
+++ play_tester.py
@@ -10,10 +10,66 @@ pp = pprint.PrettyPrinter(indent=4)
 
 # play testing section
 print("\n-----Wave 01 test data-----")
-pp.pprint(HORROR_1)
-pp.pprint(FANTASY_1)
-pp.pprint(FANTASY_2)
+# pp.pprint(HORROR_1)
+# pp.pprint(FANTASY_1)
+# pp.pprint(FANTASY_2)
 
+movie_title = "Title A"
+genre = "Horror"
+rating = 8
+
+user_data = {
+        "watched": []
+    }
+
+def create_movie(title, genre, rating):
+    
+    if title and genre and rating:
+        new_movie = {}
+        new_movie["title"] = title
+        new_movie["genre"] = genre
+        new_movie["rating"] = rating
+        return new_movie
+    else:
+        return None
+
+def add_to_watched(user_data, movie):
+
+    user_data["watched"].append(movie)
+    return user_data
+
+new_dict = create_movie(movie_title, genre, rating)
+# print(new_dict)
+# print(add_to_watched(user_data, new_dict))
+
+
+janes_data = {
+        "watchlist": [{
+            "title": MOVIE_TITLE_1,
+            "genre": GENRE_1,
+            "rating": RATING_1
+        }],
+        "watched": []
+    }
+def watch_movie(user_data, movie_title):
+
+    for index in range(0, len(user_data["watchlist"])):
+        nested_movie_title_watched = user_data["watchlist"][index]["title"]
+        if movie_title in nested_movie_title_watched:
+            correct_movie = user_data["watchlist"][index]
+            user_data["watched"].append(correct_movie)
+        user_data["watchlist"].remove(correct_movie)
+        
+    print(user_data["watchlist"])
+    print(user_data["watched"])
+    return user_data
+
+
+test = watch_movie(janes_data, MOVIE_TITLE_1)
+# print(test)
+
+
+# print(test)
 # print("\n-----Wave 02 user_data-----")
 # pp.pprint(clean_wave_2_data())
 
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,11 +1,70 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    
+    if title and genre and rating:
+        new_movie = {}
+        new_movie["title"] = title
+        new_movie["genre"] = genre
+        new_movie["rating"] = rating
+        return new_movie
+    else:
+        return None
+
+def add_to_watched(user_data, movie):
+
+    user_data["watched"].append(movie)
+    return user_data
+    
+def add_to_watchlist(user_data, movie):
+
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, movie_title):
+    for index in range(0, len(user_data["watchlist"])):
+        nested_movie_title_watched = user_data["watchlist"][index]["title"]
+        if movie_title in nested_movie_title_watched:
+            correct_movie = user_data["watchlist"][index]
+            user_data["watched"].append(correct_movie)
+            user_data["watchlist"].remove(correct_movie)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+
+    if user_data["watched"]:
+        rating_list = []
+        for index in range(0, len(user_data["watched"])):
+            rating= user_data["watched"][index]["rating"]
+            rating_list.append(rating)
+            average = sum(rating_list)/ len(rating_list)
+        return average
+    else:
+        return 0.0
+
+def get_most_watched_genre(user_data):
+
+    if user_data["watched"]:
+        genre_freq = {}
+        for index in range(0, len(user_data["watched"]) -1):
+            genre_name = user_data["watched"][index]["genre"]
+            if genre_freq and genre_name in genre_freq:
+                genre_freq[genre_name] += 1
+            else:
+                genre_freq[genre_name] = 1
+        most_watched = max(genre_freq.values())
+
+        for key in genre_freq:
+            if genre_freq[key] == most_watched:
+                return key
+    else:
+        return None
+
+
+
 
 
 # -----------------------------------------
