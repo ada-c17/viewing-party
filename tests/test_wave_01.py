819e1b849231cdb4775e820ee237dfae88f125ca
Morgan Adkisson <adkissonmorgan@gmail.com>
2022-03-18 15:21:36 -0700

    refactored individual argument if statements into one in create_movie and changed -is- to == in wave 1 tests

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -4,7 +4,7 @@ import pytest
 from viewing_party.party import *
 from tests.test_constants import *
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_create_successful_movie():
     # Arrange
     movie_title = MOVIE_TITLE_1
@@ -15,11 +15,11 @@ def test_create_successful_movie():
     new_movie = create_movie(movie_title, genre, rating)
 
     # Assert
-    assert new_movie["title"] is MOVIE_TITLE_1
-    assert new_movie["genre"] is GENRE_1
+    assert new_movie["title"] == MOVIE_TITLE_1
+    assert new_movie["genre"] == GENRE_1
     assert new_movie["rating"] == pytest.approx(RATING_1)
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_create_no_title_movie():
     # Arrange
     movie_title = None
@@ -32,7 +32,7 @@ def test_create_no_title_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_create_no_genre_movie():
     # Arrange
     movie_title = "Title A"
@@ -45,7 +45,7 @@ def test_create_no_genre_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_create_no_rating_movie():
     # Arrange
     movie_title = "Title A"
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,19 +1,25 @@
 # ------------- WAVE 1 --------------------
 
+from time import monotonic
+
+
 def create_movie(title, genre, rating):
     movie_dict = {}
-    if title:
+
+    if title and genre and rating:
         movie_dict['title'] = title 
+        movie_dict['genre'] = genre
+        movie_dict['rating'] = rating
+    else:
+        return None
 
-    if genre:
-        movie_dict['genre'] = genre 
-
-    if rating:
-        movie_dict['rating'] = rating 
-    
-    print('WORKKKKK')
-    print(movie_dict)
     return movie_dict
+
+# def add_to_watched(user_data, movie):
+#     user_data['watched'].append(movie['title'])
+#     user_data['watched'].append(movie['genre'])
+#     user_data['watched'].append(movie['rating'])
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
