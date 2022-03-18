dd1b54b3e087958de82b668791810f4548a2e40a
Amel <amelntamark@gmail.com>
2022-03-17 17:20:33 -0700

    party.py now moves movie from watchlist to watched

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -17,6 +17,14 @@ def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
 
+def watch_movie(user_data, movie):
+    for i in range(len(user_data["watchlist"])):
+        if user_data["watchlist"][i]["title"] == movie:
+            movie_dict = user_data["watchlist"].pop(i)
+            user_data["watched"].append(movie_dict)
+    return user_data
+
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
