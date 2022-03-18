47a08db557d5786ce5cb6343d91eac64e8a1e868
Grace <gpark439@gmail.com>
2022-03-18 11:30:09 -0700

    Pass test create no rating movie

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,7 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    if title == None:
+    if title == None or genre == None:
         return None
     else: 
         movie_dict = {
