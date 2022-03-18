ce9e911eb20991e2a6ddfd4167e3b7c7257432e4
Julie <jawarren89@gmail.com>
2022-03-17 21:56:03 -0700

    Changed nothing(?), passes wave 1 / test 9

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -156,7 +156,7 @@ def test_moves_movie_from_watchlist_to_watched():
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
 
-@pytest.mark.skip()
+# Test 9
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
