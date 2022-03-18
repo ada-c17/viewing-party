88b83ad3eb6a937a75e4305608313d1a468f1ded
maria <camilatagleo@gmail.com>
2022-03-18 11:15:28 -0700

    First three tests passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,24 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    new_movie = {"title": title,
+                "genre": genre,
+                "rating": rating
+                }
+    if title is None or genre is None or rating is None:
+        return None
+    return new_movie
+
+
+
+def add_to_watched(user_data,movie):
+
+
+def add_to_watchlist(user_data,movie):
+
+
+def 
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
