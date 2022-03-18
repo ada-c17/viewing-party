da14213ec91c2183c4a4162ae852aefaddb58514
Nikki Torab <nikkitorab@gmail.com>
2022-03-18 13:12:48 -0700

    finished wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -42,6 +42,55 @@ def watch_movie(user_data, title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    avg = 0
+    counter = 0
+    #for k,v in user_data["watched"]:
+    for i in user_data["watched"]:
+        for k,v in i.items():
+            if k == "rating":
+                avg+=v
+                counter+=1
+    if counter!=0:
+        avg = avg/counter
+    return avg
+
+
+def get_most_watched_genre(user_data):
+
+    if len(user_data["watched"]) == 0:
+        return None
+
+    genre_dict = {}
+    for i in user_data["watched"]:
+
+            for k,v in i.items():
+
+                if k == "genre":
+
+                    key_found = False
+                    for key in genre_dict.keys():
+
+                        if key == v:
+
+                            genre_dict[key]+=1
+
+                            key_found = True
+
+                    if key_found == False:
+                        genre_dict[v] = 1
+
+    most_watched_num = 0
+    most_watched_genre = ""
+    for g,f in genre_dict.items():
+
+        if f>most_watched_num:
+            most_watched_genre = g
+            most_watched_num = f
+
+    return most_watched_genre
+        
+
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
