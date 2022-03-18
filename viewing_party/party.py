aa368eb7bdabab3053e703597234986ab7005bd9
Uyen Vong <uvong92@gmail.com>
2022-03-17 20:56:47 -0700

    use append in add to watched and add to watchlist functions

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -18,11 +18,11 @@ def create_movie(title, genre, rating):
     return new_movie
 
 def add_to_watched(user_data, movie):
-    user_data["watched"] += [movie]
+    user_data["watched"].append(movie)
     return user_data
     
 def add_to_watchlist(user_data, movie):
-    user_data["watchlist"] += [movie]
+    user_data["watchlist"].append(movie)
     return user_data
 
 def watch_movie(user_data, title):
