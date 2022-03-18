330e22036c9ab4cb3aabf1132d5b539443e590df
Julie <jawarren89@gmail.com>
2022-03-17 21:36:43 -0700

    Cleaned up code to pass wave 1 / test 8

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -149,6 +149,8 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) is 1
     assert len(updated_data["watched"]) is 2
+    assert updated_data["watched"][-1]["title"] is movie_to_watch["title"]
+
     
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
