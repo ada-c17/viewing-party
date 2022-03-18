56489b46d2ac648ab95c9c871cb25806b54f516c
Ada <adabarries@gmail.com>
2022-03-18 11:48:20 -0700

    refactored add_to_watched() and add_to_watchlist().

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -11,17 +11,26 @@ def create_movie(title, genre, rating):
     return movie_dict
 
 def add_to_watched(user_data, movie):
-    # user_data = {}
-    # movie_list = []
-    # for movie_watched in movie:
-    #     movie_list.append(movie)
-    # user_data["watched"] = movie
-    user_data = { 
-        "watched": []
-    }
+    # user_data = { 
+    #    "watched": []
+    # }
     user_data["watched"].append(movie)
     return user_data
 
+def add_to_watchlist(user_data, movie):
+    # user_data = {
+    #     "watchlist": []
+    # }
+    user_data["watchlist"].append(movie)
+    return user_data
+
+def watch_movie(user_data, title):
+    if title in user_data["watchlist"]:
+        user_data["watchlist"].pop(title)
+        user_data["watched"].append(title)
+        return user_data
+    else:
+        return user_data    
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
