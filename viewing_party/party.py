aa995040217b19b42a5c011937deeaf93faaed89
Raha <raha.mora1@gatech.edu>
2022-03-19 00:17:53 -0400

    first wave passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,40 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    movie_table = {}
+    if title and genre and rating:
+        movie_table["title"] = title
+        movie_table["genre"] = genre
+        movie_table["rating"] = rating
+        return movie_table
+def add_to_watched(user_data,movie):
+        if not all((user_data,movie)):
+            return None   
+        watched = user_data["watched"]
+        watched.append(movie)
+        print(user_data) 
+        return user_data
+        # return updated_datas
+def add_to_watchlist(user_data,movie):
+        if not all((user_data,movie)):
+            return None
+        watchlist = user_data["watchlist"]
+        watchlist.append(movie)
+        return user_data
+def watch_movie(user_data, title):
+    watchlist = user_data["watchlist"]
+    new_watchlist = []
+    watched = user_data["watched"]
+    
+    for item in range(len(list(watchlist))):
+        if watchlist[item]["title"] != title:
+            new_watchlist.append(watchlist[item])
+        else:
+            watched.append(title)
+    user_data["watchlist"] = new_watchlist
+
+    return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
