95f59383b835c87a931723c9e828cce1b3a84aaf
Natascha <njenkns@gmail.com>
2022-03-18 17:52:50 -0700

    completed the watch movie function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -4,7 +4,7 @@
 
 def create_movie(title, genre, rating):
     # creating a dictionary that stores the variables passed into the function as the values
-    new_movie = {
+    new_movie_added = {
         "title": title,
         "genre": genre,
         "rating": rating
@@ -13,36 +13,38 @@ def create_movie(title, genre, rating):
     if title is None or genre is None or rating is None:
         return None
     else:
-        return new_movie
+        return new_movie_added
 
 def add_to_watched(user_data, movie):
-    # create a dict with "watched" as key and empty list as value
-    watched_movies = {
-        "watched": []
-    }
+    watched_movies = user_data
     # checks to see if list is empty, if it is empty, it returns empty list. If not empty, continues with conditional and appends movie dictionary to value in watched_movies dictionary
-    if not user_data:
-        return user_data
+    if not watched_movies:
+        return watched_movies
     else:
         watched_movies["watched"].append(movie)
     
     return watched_movies
 
 def add_to_watchlist(user_data, movie):
-    # create a dict with "watchlist" as key and empty list as value
-    movies_to_watch = {
-        "watchlist": []
-    }
+    movies_to_watch = user_data
 
     # checks to see if list is empty, if it is empty, it returns empty list. If not empty, continues with conditional and appends movie dictionary to value in movies_to_watch dictionary
-    if not user_data:
-        return user_data
+    if not movies_to_watch:
+        return movies_to_watch
     else:
         movies_to_watch["watchlist"].append(movie)
     
     return movies_to_watch
 
+def watch_movie(user_data, title):
+    all_movies = user_data
+    
+    for i in range(len(user_data["watchlist"])):
+        if title == all_movies["watchlist"][i]["title"]:
+            all_movies["watchlist"].remove(all_movies["watchlist"][i])
+            all_movies["watched"].append(title)
 
+    return all_movies
 
 
 # -----------------------------------------
