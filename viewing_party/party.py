039c815a7c677551a36c37a42380b3c331e40fe6
Megan Korling <korlingm@gmail.com>
2022-03-18 22:33:01 -0700

    refactored get_friends_unique_watched

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -74,11 +74,12 @@ def get_unique_watched(user_data):
     # add user's list of watched movies
     user_watched_list = user_data["watched"]
 
-    # each friend is an index holding a dictionary
+    # each index is a "friend" holding a dictionary
     friends_list = user_data["friends"]
     friends_watched_list = []
     # friend is dictionary of movie lists for each friend (keys: watched, watchlist)
     for friend in friends_list:
+        # keep it at one level
         friends_watched_list += friend["watched"]
 
     unique_list = []
@@ -88,21 +89,14 @@ def get_unique_watched(user_data):
     return unique_list
 
 def get_friends_unique_watched(user_data):
-    # add user's list of watched movies
     user_watched_list = user_data["watched"]
-
-    #names of friends
     friends_list = user_data["friends"]
-    friends_watched_list = []
-    # friend is dictionary of movie lists for each friend (watched and watchlist)
-    for friend in friends_list:
-        friends_watched_list += friend["watched"]
-
     unique_list = []
-    for movie in friends_watched_list:
-        if movie not in user_watched_list and movie not in unique_list:
-            unique_list.append(movie)
-    
+
+    for friend in friends_list:
+        for movie in friend["watched"]:
+            if movie not in user_watched_list and movie not in unique_list:
+                unique_list.append(movie)
     return unique_list
 
 # -----------------------------------------
@@ -138,14 +132,14 @@ def get_new_rec_by_genre(user_data):
     if not user_data.get("subscriptions"):
         user_data["subscriptions"] = []
 
-    not_by_genre_recs = get_available_recs(user_data) 
-    recs_list = []
+    all_recs_list = get_available_recs(user_data) 
+    genre_recs_list = []
 
-    for movie in not_by_genre_recs:
+    for movie in all_recs_list:
         if movie["genre"] == fav_genre:
-            recs_list.append(movie)
+            genre_recs_list.append(movie)
 
-    return recs_list
+    return genre_recs_list
         
 def get_rec_from_favorites(user_data):
     # user_data has key "favorites", which is a list of dictionaries
@@ -155,12 +149,12 @@ def get_rec_from_favorites(user_data):
     #list of dictionaries for each friend
     friends_watched_list = user_data["friends"]
     
-    for movie in user_favs:
+    for user_movie in user_favs:
         watched_movie = False
         for friend in friends_watched_list:
             for friend_movie in friend["watched"]:
-                if movie == friend_movie:
+                if user_movie == friend_movie:
                     watched_movie = True
         if not watched_movie:
-            movie_recs.append(movie)
+            movie_recs.append(user_movie)
     return movie_recs
