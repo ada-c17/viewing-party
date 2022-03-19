b847294ed3ae9f95d173b23d39607c6c2438b08f
Morgan Buntin <mmbuntin@gmail.com>
2022-03-19 16:38:29 -0400

    Made adjustments to pass first wave of tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,34 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    movie = {}
 
+    if title and genre and rating:
+        movie["title"] = title
+        movie["genre"] = genre
+        movie["rating"] = rating
+        return movie
+    else:
+        return None
+
+def add_to_watched(user_data, movie):
+    watched_list = user_data["watched"]
+    watched_list.append(movie)
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    watchlist = user_data["watchlist"]
+    watchlist.append(movie)
+    return user_data
+
+def watch_movie(user_data, movie):
+    counter = 0
+    for movie_dict in user_data["watchlist"]:
+        if movie_dict["title"] == movie:
+            user_data["watched"].append(movie_dict)
+            user_data["watchlist"].pop(counter)
+        counter += 1
+    return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
