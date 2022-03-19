09a81492d4dd918351e612b8473bf3e1b6963a5e
Diana <arantxa.codes@gmail.com>
2022-03-19 10:07:01 -0700

    Created watch_movie function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -18,6 +18,24 @@ def add_to_watched(user_data, movie):
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"].append(movie)
     return user_data
+
+#create watch_movie function with parameters user_data and title
+#create var that access watchlist and one for watched
+#loop through the titles for the movie in watchlist 
+#look for title value in watchlist and compare 
+#if found remove from watchlist and move dictionary for movie to watched
+#return user data
+def watch_movie(user_data, title):
+    watchlist = user_data['watchlist']
+    watched = user_data['watched']
+    for movie in watchlist:
+        if title == movie["title"]:
+            watchlist.remove(movie)
+            watched.append(movie)
+            return user_data
+    return user_data
+
+
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
