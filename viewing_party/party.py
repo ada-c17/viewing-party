182b6c548995096a46c92e73027e3e1c0284981e
Liqingcathy <cathy.glc@gmail.com>
2022-03-19 12:38:54 -0700

    finish wave03 function and pass tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,3 +1,4 @@
+
 # ------------- WAVE 1 --------------------
 #1
 def create_movie(title, genre, rating):
@@ -30,7 +31,7 @@ def watch_movie(user_data, title):
                 return user_data
             
     return user_data
-
+""" 
 janes_data = {
         "watchlist": [{
             'genre': 'Fantasy', 
@@ -44,17 +45,215 @@ janes_data = {
     }
 dict = watch_movie(janes_data, "It Came from the Stack Trace")        
 print(dict)     
-    
+"""
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
-
-
+#user_data is a dictionary with a "watched" list of movie dictionaries
+#{"watched":[{},{},{}]
+#is there a better way to find lengh of value
+def get_watched_avg_rating(user_data):
+    #try:
+        total = 0
+        count = 0
+        
+        for key, value in user_data.items():
+            for i in range(len(value)):
+                total += value[i]["rating"]
+                count += 1
+        if count != 0:
+            return total / count
+        else:
+            return 0.0
+    #except ZeroDivisionError:
+        #print("List is empty, the rating can't divide by 0!")
+#questions about two for loop, is it O(n^2)
+def get_most_watched_genre(user_data):
+    len_watched = 0
+    genre_frequency = {}
+    most_freq, genre_result = 0, ""
+    for value in user_data.values():
+        len_watched = len(value)
+        if len_watched == 0:
+            return None
+        else:
+            for i in range(len(value)):
+                genre = value[i]["genre"]
+                #if genre not in genre_frequency:
+                genre_frequency[genre] = genre_frequency.get(genre, 0) +1
+                #genre_frequency[genre] += 1 KeyError
+                if genre_frequency[genre] >= most_freq:
+                    most_freq, genre_result = genre_frequency[genre], genre
+    
+        return genre_result
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+#the value of user_data will be a dictionary with a "watched" list of movie dictionaries, and a "friends"
+#This represents that the user has a list of watched movies and a list of friends
+#{"watched":[{},{},{}], "friends": [{"watched":[{},{},{}]}, "watched":[{},{}]}
+#Consider the movies that the user has watched, and consider the movies that their friends have watched. 
+#Determine which movies the user has watched, but none of their friends have watched.
+def get_unique_watched(user_data):
+    user, friends = {}, []
+    user, friends =  get_unique_helper(user_data)
+    unique_movies_from_user = []
+    for value in user.values():
+        if value not in friends:
+            unique_movies_from_user.append(value)
+    return unique_movies_from_user    
 
+#Consider the movies that the user has watched, and consider the movies that their friends have watched. 
+#Determine which movies at least one of the user's friends have watched, but the user has not watched.
+def get_friends_unique_watched(user_data):
+    user, friends = {}, []
+    user, friends =  get_unique_helper(user_data)
+    unique_movies_from_friends = []
+    list_of_user_watched = []
+    
+    for value in user.values():
+        list_of_user_watched.append(value)
         
+    for dict in range(len(friends)):
+        if friends[dict] not in list_of_user_watched:
+            unique_movies_from_friends.append(friends[dict])
+    return unique_movies_from_friends            
+        
+def get_unique_helper(user_data):
+    user_movies_dict = {}
+    friends_movies_dict = {}
+    friends_list = []
+    result_dict = {}
+    for value in user_data.values():
+        if value == user_data["watched"]:
+            for i in range(len(user_data["watched"])):
+                user_movies_dict[i] = value[i]
+        elif value == user_data["friends"]:
+            for watched_key in value:#["watched":[{},{}], "watched":[{},{}]]
+                print(watched_key)
+                for movie_values_list in watched_key["watched"]:
+                        if movie_values_list not in friends_list:
+                            friends_list.append(movie_values_list)
+            
+    return user_movies_dict, friends_list
+user_data = { 
+        "watched": [
+            {"title": "The Lord of the Functions: The Fellowship of the Function","genre": "Fantasy","rating": 4.8},
+            { "title": "The Lord of the Functions: The Two Parameters","genre": "Fantasy","rating": 4.0},
+            {"title": "The Lord of the Functions: The Return of the Value","genre": "Fantasy","rating": 4.0},
+            { "title": "The JavaScript and the React","genre": "Action","rating": 2.2},
+            { "title": "Recursion","genre": "Intrigue","rating": 2.0},
+            { "title": "Instructor Student TA Manager","genre": "Intrigue","rating": 4.5}],
+        "friends": [{
+        "watched": [ 
+            {"title": "The Lord of the Functions: The Fellowship of the Function","genre": "Fantasy","rating": 4.8},
+            {"title": "The Lord of the Functions: The Return of the Value","genre": "Fantasy","rating": 4.0},
+            {"title": "The Programmer: An Unexpected Stack Trace","genre": "Fantasy","rating": 4.0},
+            {"title": "It Came from the Stack Trace","genre": "Horror", "rating": 3.5}
+        ]},
+        {"watched": 
+          [ 
+           {"title": "The Lord of the Functions: The Fellowship of the Function","genre": "Fantasy","rating": 4.8}, 
+           {"title": "The JavaScript and the React","genre": "Action","rating": 2.2},
+           {"title": "Recursion","genre": "Intrigue","rating": 2.0},
+           {"title": "Zero Dark Python","genre": "Intrigue","rating": 3.0}
+           ]
+        }
+        ]}
+        
+#get_unique_watched(user_data)          
+# def get_unique_watched(user_data):
+#     user_movies = set()
+#     friend_movies = set()
+#     list_of_dictionary_user = []
+#     list_of_dictionary_friends = []
+#     for value in user_data.values(): 
+#         if value == user_data["watched"]: #6 movies user watched
+#             #print(value)
+#             len_my_watched = len(value)
+#             #print(len_my_watched) 
+#             if len_my_watched > 0:
+#                 for i in range(len_my_watched):
+#                     #user_movies.add(value[i]["title"])
+#                     #print(user_movies)
+#                     print(value[i])
+#                     list_of_dictionary_user.append(value[i])
+#             else:
+#                 return list_of_dictionary_friends
+#         else:
+#             #have friends' list of movie dict [{"watched":[{},{},{}]}, "watched":[{},{}]}]
+#             #print(value)
+#             for dict in value:
+#                 watched_each_in_list = dict["watched"] 
+#                 len_movie = len(watched_each_in_list)
+#                 #print(len_movie) #4 movies in each friend's watched list
+#                 if len_movie > 0:
+#                     for index in range(len_movie):
+#                         #friend_movies.add(watched_each_in_list[index]["title"]) #7 unique movies in set now
+#                         #print(friend_movies)
+#                         #print(watched_each_in_list[index])
+#                         list_of_dictionary_friends.append(watched_each_in_list[index])
+                        
+#                 else:
+#                     return list_of_dictionary_user
+#     #Return a list of dictionaries, that represents a list of movies
+#     print(list_of_dictionary_user)
+#     print("----------------------")
+#     print(list_of_dictionary_friends)
+#     print()
+#     #print(list_of_dictionary_user  - list_of_dictionary_friends)
+#     return (set(list_of_dictionary_user) - set(list_of_dictionary_friends)) ####didn't print the whole {} but title TypeError: unhashable type: 'dict'
+
+# #Consider the movies that the user has watched, and consider the movies that their friends have watched. 
+# # Determine which movies at least one of the user's friends have watched, but the user has not watched.
+# def get_friends_unique_watched(user_data):
+#     user_movies = set()
+#     friend_movies = set()
+#     list_of_dictionary_user = []
+#     list_of_dictionary_friends = []
+#     for value in user_data.values(): 
+#         if value == user_data["watched"]: #6 movies user watched
+#             #print(value)
+#             len_my_watched = len(value)
+#             #print(len_my_watched) 
+#             if len_my_watched > 0:
+#                 for i in range(len_my_watched):
+#                     user_movies.add(value[i]["title"])
+#                     #print(user_movies)
+#                     list_of_dictionary_user.append(value[i])
+#             else:
+#                 return list(friend_movies)
+#         else:
+#             #have friends' list of movie dict [{"watched":[{},{},{}]}, "watched":[{},{}]}]
+#             #print(value)
+#             for dict in value:
+#                 watched_each_in_list = dict["watched"] 
+#                 len_movie = len(watched_each_in_list)
+#                 #print(len_movie) #4 movies in each friend's watched list
+#                 if len_movie > 0:
+#                     for index in range(len_movie):
+#                         friend_movies.add(watched_each_in_list[index]["title"]) #7 unique movies in set now
+#                         #print(friend_movies)
+#                         list_of_dictionary_friends.append(watched_each_in_list[index])
+                        
+#                 else:
+#                     return list(user_movies)
+#     #Return a list of dictionaries, that represents a list of movies
+#     print("user's watched")
+#     print(list_of_dictionary_user)
+#     print("----------------------")
+#     print("friend's watched")
+#     print(list_of_dictionary_friends)
+#     print()
+#     print(friend_movies-user_movies)
+    
+#     #no unique movies from friends:
+#     if friend_movies.issubset(user_movies):
+#         return []
+#     else:
+#         return friend_movies-user_movies
+
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
