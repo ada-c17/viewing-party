dd7855ff9b63394f9cef1e2f4092e4ad4280f57d
Vida Maleki <vida.basmani@gmail.com>
2022-03-19 13:46:23 -0700

    test wave number one is done

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,8 +1,35 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    new_movie = {"title": title, "genre":
+        genre, "rating": rating
+        }
+    if title is None or genre is None or rating is None:
+        return None
+    return new_movie
 
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+    return user_data
+
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+    return user_data
+
+
+
+def watch_movie(user_data, title):
+    
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watchlist"].remove(movie)
+            user_data["watched"].append(title)
+    return user_data
+
+
+    
+    
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
