370b1ecd26649969a1536ebaceaa7891fad5cc15
Tamara Bozich <myranae@pm.me>
2022-03-20 13:25:36 -0400

    passed all tests in wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -31,6 +31,32 @@ def watch_movie(user_dict, movie_title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+import operator 
+
+def get_watched_avg_rating(user_dict):
+    rating_total = 0
+    if user_dict["watched"]:
+        for i in range(len(user_dict["watched"])):
+            rating_total += user_dict["watched"][i]["rating"]
+            avg_rating = rating_total / (i + 1)
+    else:
+        avg_rating = 0
+    return avg_rating
+
+def get_most_watched_genre(user_dict):
+    if user_dict["watched"]:
+        genre_count = {}
+        
+        for i in range(len(user_dict["watched"])):
+            genre = user_dict["watched"][i]["genre"]
+            if genre in genre_count:
+                genre_count[genre] += 1
+            else:
+                genre_count[genre] = 1
+        return max(genre_count, key=lambda key:genre_count[key])
+    else:
+        return None
+    
 
 
 # -----------------------------------------
