73388db01a7cfcfa9257d35b7f6110a83ca9aaaa
Tetiana Zakipna <tetiana.zakipna@gmail.com>
2022-03-19 17:12:24 -0700

    Get unique watched function is added, all tests pass

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -4,31 +4,36 @@ from collections import Counter
 
 def create_movie(title, genre, rating):
     new_movie = {}
+
     if title == None or genre == None or rating == None:
         return None
     else:
         new_movie["title"] = title
         new_movie["genre"] = genre
         new_movie["rating"] = rating
+
     return new_movie
 
 def add_to_watched(user_data, movie):
     watched = user_data["watched"]
     watched.append(movie)
+
     return user_data
 
 def add_to_watchlist(user_data, movie):
     watchlist = user_data["watchlist"]
     watchlist.append(movie)
+
     return user_data
 
 def watch_movie(user_data, title):
     watchlist = user_data["watchlist"]
-    watched = user_data["watched"]
+    
     for movie in watchlist:
         if movie["title"] == title:
             watchlist.remove(movie)
             add_to_watched(user_data, title)
+
     return user_data
         
     
@@ -38,33 +43,60 @@ def watch_movie(user_data, title):
 # -----------------------------------------
 def get_watched_avg_rating(user_data):
     watched_movies = user_data["watched"]
+    
+    # make a list with the movies rating
     rating_list = []
     for movie in watched_movies:
        rating_list.append(movie["rating"])
+
+    # count average rating
     if len(rating_list) == 0:
         avg_rating = 0
     else:
         avg_rating = sum(rating_list)/len(rating_list)
+
     return avg_rating
 
 def get_most_watched_genre(user_data):
     watched_movies = user_data["watched"]
     genre_list = []
+
     for movie in watched_movies:
        genre_list.append(movie["genre"])
+
     if len(genre_list) == 0:
         return None
+
     most_watched_genre_dict = dict(Counter(genre_list))
     max_val = max(most_watched_genre_dict.values())
     most_watched_genre = ([key for key, val in most_watched_genre_dict.items() if val == max_val])
-    return ''.join(most_watched_genre)
+
+    return most_watched_genre[0]
 
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+def get_unique_watched(user_data):
+    watched_movies = user_data["watched"]
+    friends = user_data["friends"]
 
-        
+    # get the list of friends watched dictionaries
+    friends_watched_list = []
+    for friend in friends:
+        for film in friend["watched"]:
+            friends_watched_list.append(film)
+    
+    # get unique user movies
+    just_user_watched = []
+    for movie in watched_movies:
+        if movie not in friends_watched_list:
+            just_user_watched.append(movie)
+    return just_user_watched
+
+    
+
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
