8c5704c02ba6bc65993fc05c1b890f893d9ac4c0
Adriana Gutierrez <jerez.adriana@gmail.com>
2022-03-21 10:48:22 -0400

    completed test wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -38,6 +38,35 @@ def watch_movie(user_data, title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    total = 0
+    num_iterations = 0
+    for i in user_data["watched"]:
+        num_iterations += 1
+        total += i['rating']
+    try:
+        average = total / num_iterations
+    except(ZeroDivisionError):
+        return 0.0
+    return average
+    
+def get_most_watched_genre(user_data):
+    if user_data["watched"] == []:
+        return None
+    genre_tally = {}
+    for movie in user_data["watched"]:
+        try:
+            genre_tally[movie["genre"]] += 1
+        except (KeyError):
+            genre_tally[movie["genre"]] = 1
+    most_watched = (None, 0)
+    for item in genre_tally.items():
+        if item[1] > most_watched[1]:
+            most_watched = (item[0], item[1])
+    return most_watched[0]
+
+
+
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
