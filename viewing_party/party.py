1755bdd277130b14733fddd65bd24e31e936b841
Nikki Torab <nikkitorab@gmail.com>
2022-03-21 12:48:04 -0700

    wave4 all tests passed

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -124,6 +124,7 @@ def get_friends_unique_watched(user_data):
 
     unique_movies = []
     #COPY FRIENDS' WATCHED MOVIES
+    #deep copy??????????????????????????????????????????????????
     for friend in user_data["friends"]:
         for i in friend["watched"]:
 
@@ -150,19 +151,34 @@ def get_friends_unique_watched(user_data):
 
 
 
-
-
-
-
-
-
-
-
-
-        
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+
+def get_available_recs(user_data):
+
+    recommended_movies = []
+
+    friends_watched = get_friends_unique_watched(user_data)
+
+    for movie in friends_watched:
+        print(f"Movie: {movie}")
+
+        if movie["host"] in user_data["subscriptions"]:
+
+            recommended_movies.append(movie)
+
+    return recommended_movies
+
+    
+
+
+
+
+
+
+
+
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
