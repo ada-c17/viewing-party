ce04caa0f5d40bcc21323527c02a627dbc46ee94
Adriana Gutierrez <jerez.adriana@gmail.com>
2022-03-21 19:30:59 -0400

    Not sure why pytest is not finding tests anymore. Going to try to commit.

--- tests/test_wave_03.py
+++ tests/test_wave_03.py
@@ -2,21 +2,21 @@ import pytest
 from viewing_party.party import *
 from tests.test_constants import *
 
-@pytest.mark.skip()
+#pytest.mark.skip()
 def test_my_unique_movies():
     # Arrange
     amandas_data = clean_wave_3_data()
-
+    print(amandas_data)
     # Act
     amandas_unique_movies = get_unique_watched(amandas_data)
-
+    
     # Arrange
     assert len(amandas_unique_movies) == 2
     assert FANTASY_2 in amandas_unique_movies
     assert INTRIGUE_2 in amandas_unique_movies
     assert amandas_data == clean_wave_3_data()
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_my_not_unique_movies():
     # Arrange
     amandas_data = clean_wave_3_data()
@@ -28,7 +28,7 @@ def test_my_not_unique_movies():
     # Arrange
     assert len(amandas_unique_movies) == 0
 
-@pytest.mark.skip()
+#@pytest.mark.skip()
 def test_friends_unique_movies():
     # Arrange
     amandas_data = clean_wave_3_data()
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -65,14 +65,42 @@ def get_most_watched_genre(user_data):
             most_watched = (item[0], item[1])
     return most_watched[0]
 
-
-
-
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
+def get_unique_watched(user_data):
+    friend_movies = set()
+    for friend in user_data["friends"]:
+        for friend_movie in friend["watched"]:
+            friend_movies.add(friend_movie["title"])
+    unique_watched = []
+    for movie in user_data["watched"]:
+        if movie["title"] not in friend_movies:
+            unique_watched.append(movie)
+    return unique_watched
         
+def get_friends_unique_watched(user_data):
+    user_movie_titles = set()
+    for user_movie in user_data["watched"]
+        user_movie_titles.add(user_movie["title"])
+    friend_unique_titles = set()
+    friends_unique = []
+    for friend in user_data["friends"]:
+        for friend_movie in friend["watched"]:
+            if friend_movie["title"] in user_movie_titles:
+                continue
+            else:
+                if friend_movie["title"] in friend_unique_titles:
+                    continue
+                else:
+                    friend_unique_titles.add(friend_movie["title"])
+                    friends_unique.append(friend_movie)
+                    
+    return friends_unique
+
+
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
@@ -80,4 +108,3 @@ def get_most_watched_genre(user_data):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
