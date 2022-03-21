b05f8f5fa6eaf3a185fca44c5429d0d464069143
Danielle <dmbecker24@gmail.com>
2022-03-20 17:10:22 -0700

    Updated party.py to include functions that allow it to pass all test_wave_01.py test cases

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,43 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    movie_dict = {
+        "title":title,
+        "genre":genre,
+        "rating":rating
+    }
+
+    for val in movie_dict.values():
+        if not val:
+            return None
+
+    return movie_dict
+
+def add_to_watched(user, movie):
+
+    for key in user:
+        user[key].append(movie)
+    
+    return user
+
+def add_to_watchlist(user, movie):
+
+    for key in user:
+        user[key].append(movie)
+    
+    return user
+
+def watch_movie(user_data, movie):
+    
+    filtered_user_data = dict(user_data)
+
+    for i in range(len(user_data["watchlist"])):
+        watched_movie = user_data["watchlist"][i]["title"]
+        if movie == watched_movie:
+            filtered_user_data["watched"].append(filtered_user_data["watchlist"][i])
+            filtered_user_data["watchlist"].remove(filtered_user_data["watchlist"][i])
+
+    return filtered_user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
