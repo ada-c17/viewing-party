348ca6a7bc1092553e76f0efc747efb212a1a926
Victoria Shade <victoria.k.shade@gmail.com>
2022-03-21 04:15:45 -0700

    saving for the night

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,5 +1,8 @@
 # ------------- WAVE 1 --------------------
 
+from audioop import avg
+
+
 def create_movie(title, genre, rating):
 
     if title and genre and rating:
@@ -38,167 +41,79 @@ def add_to_watchlist(user_data, movie):
 
 def watch_movie(user_data, title):
 
-    print(user_data)
-    print(title)
-    
-    
     watchlist = user_data["watchlist"]
     watched = user_data["watched"]
 
-    # for movie in watchlist:
     watchlist_index = len([ele for ele in watchlist if isinstance(ele, dict)])
 
-    # print(watchlist_index)
-    count = 0
-    print(type(watchlist_index))
     for i in range(watchlist_index):
-        print(watchlist[i])
-        print(i)
-        print(range(watchlist_index))
-        print("Tori")
-        print(watchlist[i])
-        print(watchlist[i]["title"])
         if (watchlist[i]["title"]) == title:
-            print("test")
             watched_movie = watchlist.pop(i)
-            print(watched_movie)
-            print(watchlist)
 
             for value in user_data:
                 watched.append(watched_movie)
-                print(watched)
                 return user_data
 
         elif (watchlist[i]["title"]) != title:
-            print("elst statremnt")
-    
+            print("test") # delete but get check for errors before and after
+
     return user_data
-
-
-    
-#     del user_data("watchlist")[i]
-
-#     print(watchlist)
-#     print(watched)
-#     print(user_data)
-
-# else:
-#     return user_data
-
-    # for i in range(len(watchlist, )):
-    #     if user_watched_movie == True:
-    #         print(watchlist)
-    #         print(i)
-
-
-            #     user_watched_movie = True
-            # if user_watched_movie == True:
-
-
-
-
-            # for i, x in enumerate(watchlist):
-            #     if watchlist["title"] == title:
-            #         watched_movie = watchlist.pop(i)
-            #         print(watched_movie)
-            #         print(watchlist)
-
-    # user_data = {user_data[watchlist], user_data[watched]}
-    #    for movie in user_data["watchlist"]:
-
-    # for key, value in user_data.items():
-    #     for watchlist in value:
-    #         if title in watchlist:
-    #             del title
-
-    # for watched in value:
-    #     watched.append(title) 
-    #     print(user_data) 
-    #     return user_data
-
-                    # if title in watchlist["title"]:
-
-
-                
-    # for key, value in user_data.items():
-    #     for watchlist in value:
-    #         if watchlist["title"] == title:
-    #             print(i for i in range(len(user_data["watchlist"])))
-    #             watched_movie = watchlist.pop("title")
-    #             print(watched_movie)
-    #             print(watchlist)
-    
-
-    # for key, value in range(len(user_data.items())):
-    #     for watchlist in value:
-    #         if value["title"] == title:
-    #                 watched_movie = watchlist.pop(index)
-    #                 print(watched_movie)
-    #                 print(watchlist)
-
-
-    # for i in range(len(watchlist)):
-    #     if watchlist.items() == title:
-    #         print("Tori")
-    #         watched_movie = watchlist.pop("title")
-    #         print(watched_movie)
-    #         print(watchlist)
-
-                    
-                #     for key, value in user_data.items():
-                #         for watched in value:
-                #             watched.append(watched_movie)
-                #             return user_data
-                    
-                #     del user_data("watchlist")[i]
-
-                #     print(watchlist)
-                #     print(watched)
-                #     print(user_data)
-
-                # else:
-                #     return user_data
-
-
-
-
-
-
-# for index in range(len(my_list)): if my_list[index]['id'] == 2: del my_list[index] break print("The result is :") print(my_list)
-
-
-
-
-    #if title is in watchlist
-    #remove the movie from watchlist
-    #add movie to watched
-    #return user_data
-
-    #if title not in watchlist
-    #return user_data
-
-
-
-
-
-
-#the value of user_data will be a dictionary with a key "watched", and a value which is a list of dictionaries representing the movies the user has watched
-#An empty list represents that the user has no movies in their watched list
-#the value of movie will be a dictionary in this format:
-
-
-
-
-#If those three attributes are truthy, then return a dictionary. This dictionary should...
-# Have three key-value pairs, with specific keys
-# The three keys should be "title", "genre", and "rating"
-# The values of these key-value pairs should be appropriate values
-# If title is falsy, genre is falsy, or rating is falsy, this function should return None
-
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+
+def get_watched_avg_rating(user_data):
+
+    #watched = user_data["watched"]
+    print(user_data)
+    watched = user_data["watched"]
+    #rating = watched["rating"]
+    # print(ratings)
+
+    number_movies_watched = len([ele for ele in watched if isinstance(ele, dict)])
+    print(number_movies_watched)
+
+    # print(user_data["watched"].get("rating"))
+    list_ratings = []
+    for i in range(number_movies_watched):
+        ratings = sum([user_data["watched"][i]["rating"] for key in user_data])
+        list_ratings.append("ratings")
+
+    # total_rating = []
+
+    # for key, value in user_data.items():
+    #     for watched in value:
+    #         for rating in key:
+    #             total_rating.append(watched["rating"])
+    #             return total_rating
+            
+
+        # for key, value in user_data.items():
+        #     for watched in value:
+        #         print(watched["rating"])
+
+
+    # for i in range(number_movies_watched):
+
+                # for rating in key:
+                # for rating in value:
+
+
+#     Calculate the average rating of all movies in the watched list
+# The average rating of an empty watched list is 0.0
+# return the average rating
+
+def get_most_watched_genre(user_data):
+    pass
+
+# This represents that the user has a list of watched movies. Each watched movie has a genre.
+# The values of "genre" is a string.
+# Determine which genre is most frequently occurring in the watched list
+# return the genre that is the most frequently watched
+
+
+
 
 
 # -----------------------------------------
