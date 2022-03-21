4957d07cd87b052228b8bc98287d972367cedc08
Grace <gpark439@gmail.com>
2022-03-21 00:14:01 -0700

    Adds movie to user watched list

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,18 +1,26 @@
 # ------------- WAVE 1 --------------------
 
+from curses import keyname
+from xml.sax.handler import EntityResolver
+
+from tests.test_constants import GENRE_1, MOVIE_TITLE_1, RATING_1, USER_DATA_2
+
+
 def create_movie(title, genre, rating):
     if title == None or genre == None or rating == None:
         return None
     else: 
-        movie_dict = {
+        movie = {
             "title" : title,
             "genre" : genre,
             "rating" : rating
     }
 
-    return movie_dict
+    return movie
 
-
+def add_to_watched(user_data, movie):
+    user_data["watched"] = [movie]
+    return user_data
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
