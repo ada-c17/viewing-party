5ba12b085f26223bbd24d0c3c78b4b1a495a9512
Grace <gpark439@gmail.com>
2022-03-21 23:35:13 -0700

    Calculate average rating as zero if watched list empty

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -5,7 +5,7 @@ from xml.sax.handler import EntityResolver
 
 from tests.test_constants import GENRE_1, MOVIE_TITLE_1, RATING_1, USER_DATA_2
 
-
+# adds movie data as item in movie dictionary
 def create_movie(title, genre, rating):
     if title == None or genre == None or rating == None:
         return None
@@ -18,14 +18,17 @@ def create_movie(title, genre, rating):
 
     return movie
 
+# adds movie to watched list
 def add_to_watched(user_data, movie):
     user_data["watched"] = [movie]
     return user_data
 
+# adds movie to watchlist
 def add_to_watchlist(user_data, movie):
     user_data["watchlist"] = [movie]
     return user_data
 
+# moves watchlist movie to watched list
 def watch_movie(user_data, title):
     for movie in user_data["watchlist"]:
         for category, item in movie.items():
@@ -39,14 +42,19 @@ def watch_movie(user_data, title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+# gets average rating of movies for user
 def get_watched_avg_rating(user_data):
     ratings = []
-    for movie in user_data["watched"]: # for movie in the watched list
-        for category, item in movie.items():
-            if category == "rating":
-                ratings.append(item)   
-    average_rating = ((sum(ratings))/(len(ratings)))
+    if len(user_data["watched"]) > 0:
+        for movie in user_data["watched"]: 
+            for category, item in movie.items():
+                if category == "rating":
+                    ratings.append(item)   
+        average_rating = ((sum(ratings))/(len(ratings)))
+    else:
+        average_rating = 0
     return average_rating
+
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
