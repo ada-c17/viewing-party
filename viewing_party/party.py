b8be9a78520bd908ba55460e37016769fb6d1d7a
Fena Dang <fenadang@gmail.com>
2022-03-21 22:17:40 -0700

    completed all func and test for wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -27,6 +27,36 @@ def watch_movie(user_data, title):
             return user_data
     return user_data
 
+# -----------------------------------------
+# ------------- WAVE 2 --------------------
+# -----------------------------------------
+
+def get_watched_avg_rating(user_data):
+    if not user_data["watched"]:
+        return 0
+    else:
+        avg_list = []
+        for i in range(len(user_data["watched"])):
+            avg_list.append(user_data["watched"][i]["rating"])
+        return sum(avg_list) / len(avg_list)
+
+
+def get_most_watched_genre(user_data):
+    if not user_data["watched"]:
+        return None
+    else:
+        most_watched_dict = {}
+        for i in range(len(user_data["watched"])):
+            genre = user_data["watched"][i]["genre"]
+            if genre in most_watched_dict:
+                most_watched_dict[genre] += 1
+            else:
+                most_watched_dict[genre] = 1
+        return max(most_watched_dict, key=most_watched_dict.get)
+
+
+
+
 
 # janes_data = {
 #     "watchlist": [{
@@ -39,17 +69,35 @@ def watch_movie(user_data, title):
 #         "genre": "e",
 #         "rating": "f"
 #     }],
-#     "watched": ["p"]
+#     "watched": [{
+#         "title": "aa",
+#         "genre": "bb",
+#         "rating": "cc"
+#     }, 
+#     {
+#         "title": "dd",
+#         "genre": "ee",
+#         "rating": "ff"
+#     },
+#     {
+#         "title": "aa",
+#         "genre": "bb",
+#         "rating": "cc"
+#     }, 
+#     {
+#         "title": "dd",
+#         "genre": "bb",
+#         "rating": "ff"
+#     }]
 # }
-# print(watch_movie(janes_data, "d"))
+
+# test_most_watched_genre(janes_data)
 
 
 
 
 
-# -----------------------------------------
-# ------------- WAVE 2 --------------------
-# -----------------------------------------
+
 
 
 # -----------------------------------------
