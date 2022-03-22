a97f73c81e51e6a45c147886eae936dff3abde6e
Amber <ambertanaka@yahoo.com>
2022-03-21 21:23:30 -0700

    passing 13 tests wave 2 done

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -33,6 +33,29 @@ def watch_movie(user_data, title):
             user_data["watchlist"].remove(movie)
     return user_data
 
+def get_watched_avg_rating(user_data):
+    total = 0
+    for movie in user_data["watched"]:
+        total += movie["rating"]
+    if len(user_data["watched"]) <= 0:
+        average = 0.0
+        return average
+    elif len(user_data["watched"]) > 0:
+        average = total / len(user_data["watched"])
+        return average
+
+
+def get_most_watched_genre(user_data):
+    popular_genre = {}
+    if len(user_data["watched"]) == 0:
+        return None
+    for movie in user_data["watched"]:
+        if movie["genre"] not in popular_genre:
+            popular_genre[movie["genre"]] = 1
+        else:
+            popular_genre[movie["genre"]] += 1
+    best_genre = max(popular_genre, key = popular_genre.get)
+    return best_genre
 
 
 
