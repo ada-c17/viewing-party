4fbb4d006af4cc4d6288e3fa04ac5ef5b8623100
Hope Olaide <hopeolaide@gmail.com>
2022-03-22 16:08:41 -0400

    I have worked through most of Wave 1.

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,51 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    
+    if not title or not genre or not rating:
+        return None
+    else:
+        new_movie = {
+        "title": title,
+        "genre": genre,
+        "rating": rating
+    }
+        
+    return new_movie
+
+def add_to_watched(user_data, movie):
+    
+    user_data["watched"].append(movie)          # updated_data = user_data.update({"watched": movie})
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+     user_data["watchlist"].append(movie)
+     return user_data 
+
+def watch_movie(user_data, title):
+    # for title in user_data["watchlist"][0].values():    #Figure out how to make it dynamic. 
+    for movie in range(len(user_data["watchlist"])): 
+        # print(f"HERE: {title}")
+        if title in user_data["watchlist"][movie].values():            
+            user_data = add_to_watched(user_data, user_data["watchlist"][title])
+            user_data["watchlist"].pop(title)
+            
+        else:
+            len(["watchlist"]) == 0
+            
+    return user_data 
+    
+# example_dict[ key_to_find_inner_list ][ index_to_find_element ]
+
+    janes_data = {
+        "watchlist": [{
+            "title": MOVIE_TITLE_1,
+            "genre": GENRE_1,
+            "rating": RATING_1
+        }],
+        "watched": []
+    }
+# new_movie = create_movie(movie_title, genre, rating)
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
@@ -12,7 +56,7 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-        
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
@@ -21,3 +65,12 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 
+# def create_movie(title, genre, rating):
+#     movies = {}
+#     if title and genre and rating: 
+#         movies["title"] = title
+#         movies["genre"] = genre
+#         movies["rating"] = rating
+#         return movies
+#     else:
+#         return None
