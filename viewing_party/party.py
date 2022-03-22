aab30f3d9a2c2aeb63dff608850c4cb0659980b3
Nishat <nsalsabil25@gmail.com>
2022-03-22 11:07:39 -0700

    added 2 functions in party.py to pass first 5 tests in wave_01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,35 @@
 # ------------- WAVE 1 --------------------
 
-def create_movie(title, genre, rating):
-    pass
+from sys import argv
+
+
+def create_movie(movie_title, genre, rating):
+    dict = {}
+
+    if movie_title == None or genre == None or rating == None:
+        return None 
+    else:
+        dict["title"] = movie_title
+        dict["genre"] = genre
+        dict["rating"] = rating
+
+    return dict 
+
+
+def add_to_watched(user_data, create_movie):
+    user_data = {}
+    list = []
+    user_data["watched"] = list
+    list.append(create_movie)
+
+    # if user_data == None:
+    #     return dict["watched"] == [] 
+    # else:
+    #     dict["watched"] = list_movie
+
+    return user_data
+
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
