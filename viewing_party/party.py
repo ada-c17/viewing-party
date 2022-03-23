de00e05399390f45b77f6209cd390ba158896ab8
Tiffini Hyatt <tiffini.hyatt@gmail.com>
2022-03-22 20:51:49 -0500

    Passes wave 2 get_watched_avg_rating function

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -35,8 +35,8 @@ def add_to_watchlist(user_data, movie):
 
 def watch_movie(user_data, movie_title):
     # this function will loop through the list user_data["watchlist"]
-    # if user_data["watch_ist"][i]["title"] == movie_title, then 
-    # that inner dictionary will be added to user_data["watched"] user_data["watched"].append(user_data[watchlist][i])
+    # if user_data["watch_ist"][i]["title"] == movie_title, 
+    # that inner dictionary will be added to user_data["watched"]
     # and be removed from user_data["watchlist"]
 
     for movie in user_data["watchlist"]:
@@ -49,6 +49,22 @@ def watch_movie(user_data, movie_title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+
+def get_watched_avg_rating(user_data):
+    # this function accesses the ratings for all of the movies in watched
+    # and return the average of those ratings
+
+    total = 0
+
+    for movie in user_data["watched"]:
+        # convert string ratings to float to allow for math operations
+        float_rating = float(movie["rating"])
+        total += float_rating
+    
+    # calculate average rating for all movies within "watched"
+    rating_average = total/len(user_data["watched"])
+
+    return rating_average
 
 
 # -----------------------------------------
