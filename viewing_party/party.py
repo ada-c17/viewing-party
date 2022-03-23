244ac8aa1381bf2cf89a4886825e59a6272ffe61
mit <mit.magh@gmail.com>
2022-03-22 19:27:30 -0700

    End of Wave 1

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,12 +1,47 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    new_movie = {
+        "title":title,
+        "genre":genre,
+        "rating":rating
+    }
+    if title == None or genre == None or rating == None:
+        return None
+    return new_movie
+
+def add_to_watched(user_data, movie):
+    if user_data == None or movie == None:
+        return None
+
+    user_data["watched"].append(movie)
+    return user_data
+    
+def add_to_watchlist(user_data, movie):
+    if user_data == None or movie == None:
+        return None
+
+    user_data["watchlist"].append(movie)
+    return user_data
+    
+def watch_movie(user_data, title):
+    if user_data == None or title == None or not type(title)==str :
+        return None
+    watchlist = user_data["watchlist"]
+    watched = user_data["watched"]
+    for item in range(len(watchlist)):
+        if title == watchlist[item]["title"]:
+            watched.append(watchlist[item])
+            watchlist.remove(watchlist[item])
+        
+    return user_data
+    
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
-
+def get_watched_avg_rating():
+    
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
