e9aa8bfe99c496d0c8f37e0025d971b2f5a156a2
cathos <cathos@startmail.com>
2022-03-23 14:45:05 -0700

    FInished wave 2

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,5 +1,8 @@
 # ------------- WAVE 1 --------------------
 
+from statistics import mean
+
+
 def create_movie(title, genre, rating):
     if None in (title,genre,rating,):
         return None
@@ -30,17 +33,54 @@ def watch_movie(user_data, title):
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    try: 
+        filter_ratings = []
+        for movie in user_data["watched"]:
+            filter_ratings.append(movie["rating"])
+        average_rating = mean(filter_ratings)
+        return average_rating
+    except:
+        return 0.0
+
+def get_most_watched_genre(user_data):
+    genre_count = {}
+    genre_counter = 0
+    top_genre = None
+    for movie in user_data["watched"]:
+        if movie["genre"] in genre_count:
+            genre_count[movie["genre"]] += 1
+        else: 
+            genre_count[movie["genre"]] = 1
+        if genre_count[movie["genre"]] > genre_counter:
+            top_genre = movie["genre"]
+            genre_counter = genre_count[movie["genre"]]
+    return top_genre 
+
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-        
+def get_unique_watched(user_data):
+    pass
+
+def get_friends_unique_watched(user_data):
+    pass
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+
+def get_available_recs(user_data):
+    pass
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
 
+def get_new_rec_by_genre(user_data):
+    pass
+
+def get_rec_from_favorites(user_data):
+    pass
