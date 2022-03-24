c17e5dfc5ef7a5d20c7a1c736800f9d3037e0414
Marjana Khatun <marjana1816@gmail.com>
2022-03-23 23:34:29 -0700

    add test codes

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,16 +1,112 @@
 # ------------- WAVE 1 --------------------
+from tests.test_constants import *
+def create_movie(movie_title, genre, rating):
+    if movie_title == None or genre == None or rating == None:
+        return None
+    else:
+        new_movie = {"title":MOVIE_TITLE_1,"genre":GENRE_1,"rating":RATING_1}
+        return new_movie
 
-def create_movie(title, genre, rating):
-    pass
+def add_to_watched(user_data, movie):
+    user_data = {"watched":[{"title":MOVIE_TITLE_1, "genre":GENRE_1, "rating":RATING_1}]}
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data = {"watchlist": [{"title":MOVIE_TITLE_1,"genre":GENRE_1,"rating":RATING_1}]}
+    return user_data
+
+def watch_movie(janes_data, movie_title1):
+
+    movie_data = {"title": movie_title1 , "genre": GENRE_1, "rating": RATING_1 }
+    for i in janes_data["watchlist"]:
+        if movie_data in janes_data["watchlist"]:
+            janes_data["watchlist"].remove(movie_data)
+            janes_data["watched"].append(movie_data)
+            return janes_data
+    return janes_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(janes_data):
+    total_rating = 0
+    counter = 0
+    if janes_data["watched"] == []:
+        average = 0.0
+        return average
+    else:
+        for val in USER_DATA_2.values():
+            for dict in val:
+                for key in dict:
+                    if key == "rating":
+                        total_rating += dict[key]
+                        counter+= 1
+        average = total_rating/counter
+        return average 
+
+def get_most_watched_genre(janes_data):
+    
+    genre_dict ={}
+    
+    if janes_data["watched"] == []:
+        popular_genre = None
+        return popular_genre
+    else:                  
+        for watched_items in USER_DATA_2.values():
+            for movie_data in watched_items:
+                key = movie_data["genre"]
+                if key not in genre_dict:
+                    genre_dict[key]= 1 
+                else:
+                    genre_dict[key] +=1
+    popular_genre = max(genre_dict,key=genre_dict.get)
+    return popular_genre
+    
+        
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+def get_unique_watched(amandas_data):
+    amandas_unique_movies =[]
+    list_friends = []
+    list_amandas = []
+
+    for element in amandas_data["friends"]:
+        for movie_data in element["watched"]:
+            list_friends.append(movie_data)
+    
+    
+    for element in amandas_data["watched"]:
+        list_amandas.append(element)
+
+    
+    for element in list_amandas:
+        if element not in list_friends:
+            amandas_unique_movies.append(element)
+
+    return amandas_unique_movies
+
+def get_friends_unique_watched(amandas_data):
+    friends_unique_movies =[]
+    list_friends = []
+    list_amandas = []
+
+    for element in amandas_data["friends"]:
+        for movie_data in element["watched"]:
+                list_friends.append(movie_data)
+    
+    
+    for element in amandas_data["watched"]:
+            list_amandas.append(element)
+
+
+    for element in list_friends:
+        if element not in list_amandas:
+            friends_unique_movies.append(element)
+   
+    return friends_unique_movies
 
         
 # -----------------------------------------
