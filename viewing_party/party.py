8fc440f34ebb7b0106e7177d2933aab848c2a851
Nishat <nsalsabil25@gmail.com>
2022-03-24 15:41:23 -0700

    tests up to 20 are passing and wave_04 complete

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -122,16 +122,24 @@ def get_friends_unique_watched(user_data):
 
     return movies_not_watched_by_user
 
-                
-
-
-
 
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
 
+def get_available_recs(user_data): 
+    user_host_recs = []
+    friends_list = get_friends_unique_watched(user_data)
+    for i in friends_list:
+        if i["host"] in user_data["subscriptions"]:
+            user_host_recs.append(i)
+    return user_host_recs
+     
+
+    
+
+    
+
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
