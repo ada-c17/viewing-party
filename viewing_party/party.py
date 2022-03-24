3c56528b8f86a86e6922f672b6567f19078875a3
Joanna <joannaparis7@gmail.com>
2022-03-23 20:06:10 -0400

    Passed some wave 3 tests

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -101,18 +101,38 @@ def get_most_watched_genre(user_data):
     return max_genre
     
     
-            
-    
-
-
-
-
-
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+'''
+- the value of `user_data` will be a dictionary with a `"watched"` list of movie dictionaries, and a `"friends"`
+    - This represents that the user has a list of watched movies and a list of friends
+    - The value of `"friends"` is a list
+    - Each item in `"friends"` is a dictionary. This dictionary has a key `"watched"`, which has a list of movie dictionaries.
+    - Each movie dictionary has a `"title"`.
+- Consider the movies that the user has watched, and consider the movies that their friends have watched. Determine which movies the user has watched, but none of their friends have watched.
+- Return a list of dictionaries, that represents a list of movies
+'''
 
+def get_unique_watched(user_data):
+    friends_movie_list = []
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie not in friends_movie_list:
+                friends_movie_list.append(movie)
+    watched_list = []
+    for movie in user_data["watched"]:
+        if movie not in friends_movie_list:
+            watched_list.append(movie)
+    return watched_list
+                
+            
         
+        
+def get_friends_unique_watched(user_data):
+    pass
+
+    
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
