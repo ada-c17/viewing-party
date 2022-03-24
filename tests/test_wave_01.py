c14eaccbcd01e55387b151ecb1fdd068fbf8ced5
Marlyn <marlynlopezz@gmail.com>
2022-03-24 16:45:01 -0700

    adding assert to watchlist func

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -141,6 +141,7 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
+    assert updated_data["watched"][0]["title"] == MOVIE_TITLE_1
     
     
     # *******************************************************************************************
