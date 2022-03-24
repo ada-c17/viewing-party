d7c391ec7b4bb60e24ca4e02af601d9da4fe0ca3
Nina <ninagetsbored@gmail.com>
2022-03-23 23:20:01 -0700

    passed test_wave_01

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,7 +1,41 @@
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
-    pass
+    movie = {}
+
+    if title and genre and rating:
+        movie["title"] = title
+        movie["genre"] = genre
+        movie["rating"] = rating
+    else:
+        return None
+
+    return movie
+
+def add_to_watched(user_data, movie):
+    user_data["watched"].append(movie)
+
+    return user_data
+
+def add_to_watchlist(user_data, movie):
+    user_data["watchlist"].append(movie)
+
+    return user_data
+
+def watch_movie(user_data, title):
+    watched_movie = {}
+
+    for list, movie_data in user_data.items():
+        for data in movie_data:
+            if data["title"] == title:
+                watched_movie = data
+                user_data["watchlist"].remove(data)
+
+    if len(watched_movie) >= 1:
+        add_to_watched(user_data, watched_movie)
+
+    return user_data
+
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
@@ -12,7 +46,7 @@ def create_movie(title, genre, rating):
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
-        
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
@@ -20,4 +54,3 @@ def create_movie(title, genre, rating):
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
-
