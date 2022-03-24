fb559301d2aac78af6982254cb8f82c57e009102
maria <camilatagleo@gmail.com>
2022-03-23 22:56:20 -0700

    Al tested passed, some assertions done

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -118,7 +118,10 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) is 0
     assert len(updated_data["watched"]) is 1
-    
+    # *******************************************************************************************
+    assert updated_data["watched"][0]["title"] == MOVIE_TITLE_1
+    assert updated_data["watched"][0]["genre"] == GENRE_1
+    assert updated_data["watched"][0]["rating"] == RATING_1
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
@@ -141,7 +144,9 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) is 1
     assert len(updated_data["watched"]) == 2
-    
+    # *******************************************************************************************
+    assert updated_data["watched"][1] == HORROR_1
+
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
