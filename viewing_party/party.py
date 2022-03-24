6b13092523545fb7639345ff6226806b09cc39b0
Nina <ninagetsbored@gmail.com>
2022-03-23 23:39:11 -0700

    passed test_wave_02

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -36,16 +36,51 @@ def watch_movie(user_data, title):
 
     return user_data
 
-
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
 
+def get_watched_avg_rating(user_data):
+    rating_sum = 0
+    rating_count = 0
+
+    for list, movie_data in user_data.items():
+        for data in movie_data:
+            rating_sum += data["rating"]
+            rating_count += 1
+
+    if len(user_data["watched"]) >= 1:
+        average_rating = rating_sum / rating_count
+        return average_rating
+    else:
+        return 0
+
+def get_most_watched_genre(user_data):
+    genre_counter = {}
+
+    for list, movie_data in user_data.items():
+        for data in movie_data:
+            genre = data["genre"]
+            if genre in genre_counter:
+                genre_counter[genre] += 1
+            else:
+                genre_counter[genre] = 1
+
+    if len(genre_counter) == 0:
+        return None
+    else:
+        favorite_genre = max(genre_counter, key=genre_counter.get)
+        return favorite_genre
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
 
+def get_unique_watched(user_data):
+    pass
+
+def get_friends_unique_watched(user_data):
+    pass
 
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
