30073495ac74382b5cf127859b843d2ebf676754
Andrea Garcia <andrygzt@gmail.com>
2022-03-24 02:00:28 -0700

    when wave02 passes

--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,3 +1,6 @@
+from doctest import ELLIPSIS
+from pickle import NONE
+
 # ------------- WAVE 1 --------------------
 
 def create_movie(title, genre, rating):
@@ -27,6 +30,45 @@ def watch_movie(user_data, title):
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+
+def get_watched_avg_rating(user_data):
+    addition = 0
+    if len(user_data["watched"])>=1: 
+        for num_movies in range(0,len(user_data["watched"])):
+            addition += user_data["watched"][num_movies]["rating"]
+        average = addition/ len(user_data["watched"])
+    else:
+        average = 0.0
+    return average
+
+
+def get_most_watched_genre(user_data):
+    genre_dict = {}
+    
+    
+    for movie in user_data["watched"]:
+        genre_var = movie["genre"]
+        if genre_var in genre_dict:
+            genre_dict[genre_var] +=1
+        else:
+            genre_dict[genre_var] = 1   
+
+    popular_genre = None
+    counter_genre = 0
+    for genre in genre_dict:
+        if genre_dict[genre] > counter_genre:
+            popular_genre = genre
+            counter_genre = genre_dict[genre] 
+            
+
+    return popular_genre
+
+        
+
+
+    
+        
+        
 
 
 # -----------------------------------------
