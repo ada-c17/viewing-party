9dca9434abdcdd6b7c185c471f906b167bbf0aa0
Shannon Bellemore <shannonbellemore@gmail.com>
2022-03-25 04:00:58 -0500

    Viewing-Party project -Shannon Bellemore

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -1,10 +1,11 @@
+from operator import ge
 import pytest
 # NOTE: In production code, we developers should change import * to something more specific. Due to some constraints of this project, we will import * in our test files.
 # from viewing_party.main import *
 from viewing_party.party import *
 from tests.test_constants import *
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_create_successful_movie():
     # Arrange
     movie_title = MOVIE_TITLE_1
@@ -15,11 +16,14 @@ def test_create_successful_movie():
     new_movie = create_movie(movie_title, genre, rating)
 
     # Assert
-    assert new_movie["title"] is MOVIE_TITLE_1
-    assert new_movie["genre"] is GENRE_1
+    assert new_movie["title"] == MOVIE_TITLE_1
+    assert new_movie["genre"] == GENRE_1
     assert new_movie["rating"] == pytest.approx(RATING_1)
 
-@pytest.mark.skip()
+
+
+
+# @pytest.mark.skip()
 def test_create_no_title_movie():
     # Arrange
     movie_title = None
@@ -32,7 +36,9 @@ def test_create_no_title_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
+
+
+# @pytest.mark.skip()
 def test_create_no_genre_movie():
     # Arrange
     movie_title = "Title A"
@@ -45,7 +51,9 @@ def test_create_no_genre_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
+
+
+# @pytest.mark.skip()
 def test_create_no_rating_movie():
     # Arrange
     movie_title = "Title A"
@@ -58,7 +66,9 @@ def test_create_no_rating_movie():
     # Assert
     assert new_movie is None
 
-@pytest.mark.skip()
+
+
+# @pytest.mark.skip()
 def test_adds_movie_to_user_watched():
     # Arrange
     movie = {
@@ -75,11 +85,14 @@ def test_adds_movie_to_user_watched():
 
     # Assert
     assert len(updated_data["watched"]) is 1
-    assert updated_data["watched"][0]["title"] is MOVIE_TITLE_1
-    assert updated_data["watched"][0]["genre"] is GENRE_1
-    assert updated_data["watched"][0]["rating"] is RATING_1
+    assert updated_data["watched"][0]["title"] == MOVIE_TITLE_1
+    assert updated_data["watched"][0]["genre"] == GENRE_1
+    assert updated_data["watched"][0]["rating"] == RATING_1
 
-@pytest.mark.skip()
+
+
+
+# @pytest.mark.skip()
 def test_adds_movie_to_user_watchlist():
     # Arrange
     movie = {
@@ -96,12 +109,15 @@ def test_adds_movie_to_user_watchlist():
 
     # Assert
     assert len(updated_data["watchlist"]) is 1
-    assert updated_data["watchlist"][0]["title"] is MOVIE_TITLE_1
-    assert updated_data["watchlist"][0]["genre"] is GENRE_1
-    assert updated_data["watchlist"][0]["rating"] is RATING_1
+    assert updated_data["watchlist"][0]["title"] == MOVIE_TITLE_1
+    assert updated_data["watchlist"][0]["genre"] == GENRE_1
+    assert updated_data["watchlist"][0]["rating"] == RATING_1
 
-@pytest.mark.skip()
-def test_moves_movie_from_watchlist_to_empty_watched():
+
+
+
+# @pytest.mark.skip()
+
     # Arrange
     janes_data = {
         "watchlist": [{
@@ -116,14 +132,16 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     updated_data = watch_movie(janes_data, MOVIE_TITLE_1)
 
     # Assert
-    assert len(updated_data["watchlist"]) is 0
-    assert len(updated_data["watched"]) is 1
+    assert len(updated_data["watchlist"]) == 0
+    assert len(updated_data["watched"]) == 1
     
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
 
-@pytest.mark.skip()
+
+
+# @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
     # Arrange
     movie_to_watch = HORROR_1
@@ -139,14 +157,14 @@ def test_moves_movie_from_watchlist_to_watched():
     updated_data = watch_movie(janes_data, movie_to_watch["title"])
 
     # Assert
-    assert len(updated_data["watchlist"]) is 1
-    assert len(updated_data["watched"]) is 2
+    assert len(updated_data["watchlist"]) == 1
+    assert len(updated_data["watched"]) == 2
     
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
 
-@pytest.mark.skip()
+# @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
     # Arrange
     movie_to_watch = HORROR_1
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,23 +1,132 @@
+import statistics
+# -----------------------------------------
 # ------------- WAVE 1 --------------------
-
+# -----------------------------------------
 def create_movie(title, genre, rating):
-    pass
+    # create movie information
+    movie_info = {}
+    if title == None or genre == None or rating == None:
+        return None
+    movie_info['title'] = title
+    movie_info['genre'] = genre
+    movie_info['rating'] = rating
+    return movie_info 
+
+
+def add_to_watched(user_data, movie):
+    # add movie to what user has watched
+    user_data["watched"].append(movie)
+    return user_data
+
+
+def add_to_watchlist(user_data, movie):
+    # add movie to user's watchlist
+    user_data["watchlist"].append(movie)
+    return user_data
+
+
+def watch_movie(user_data, title):
+    # add a watched movie to watched
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            add_to_watchlist(user_data, movie)
+    # remove watched movie from watchlist
+    for movie in user_data["watchlist"]:
+        if movie["title"] == title:
+            user_data["watchlist"].remove(movie)
+    return user_data
 
 # -----------------------------------------
 # ------------- WAVE 2 --------------------
 # -----------------------------------------
+def get_watched_avg_rating(user_data):
+    combined_rating = 0
+    if user_data["watched"] == []:
+        average = 0.0
+    # calculate average of rating
+    for movie in user_data["watched"]:
+        combined_rating += movie["rating"]
+        average = (combined_rating/len(user_data["watched"]))
+    return average
 
+
+def get_most_watched_genre(user_data):
+    # make a list to store genre
+    mode_list = []
+    if len(user_data["watched"]) == 0:
+        return None
+    # use mode function to find genre mode of mode list
+    for movie in user_data["watched"]:
+        mode_list.append(movie["genre"])
+    return statistics.mode(mode_list)
 
 # -----------------------------------------
 # ------------- WAVE 3 --------------------
 # -----------------------------------------
+def get_unique_watched(user_data):
+    # make list of friends movies
+    friend_movies = []
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            friend_movies.append(movie["title"])
+    user_unique_movies = []
+    # if movie is not in friends movie list, add to user unique movies
+    for movie in user_data["watched"]:
+        if movie["title"] not in friend_movies:
+            user_unique_movies.append(movie)
+    return user_unique_movies
 
-        
+
+def get_friends_unique_watched(user_data):
+    # make list of user movies
+    user_movies = []
+    for movie in user_data["watched"]:
+        user_movies.append(movie["title"])
+    # if movie not in users movie list, add to friend unique movies
+    friend_unique_movies = {}
+    for friend in user_data["friends"]:
+        for movie in friend["watched"]:
+            if movie["title"] not in user_movies:
+                friend_unique_movies[movie["title"]] = movie
+    friend_unique = list(friend_unique_movies.values())
+    return friend_unique
+
 # -----------------------------------------
 # ------------- WAVE 4 --------------------
 # -----------------------------------------
+def get_available_recs(user_data):
+    # use helper function to get friends unique movies
+    friend_unique_movies = get_friends_unique_watched(user_data)
+    # iterate through friends unique movies and add movie to list if host is in user's subscriptions
+    recommended_movies = []
+    for movie in friend_unique_movies:
+        if movie["host"] in user_data["subscriptions"]:
+            recommended_movies.append(movie)
+    return recommended_movies
 
 # -----------------------------------------
 # ------------- WAVE 5 --------------------
 # -----------------------------------------
+def get_new_rec_by_genre(user_data):
+    user_most_watched_genre = get_most_watched_genre(user_data)
+    friend_unique_movies = get_friends_unique_watched(user_data)
+    user_recommended_movies = []
+    for movie in friend_unique_movies:
+        if movie["genre"] == user_most_watched_genre:
+            user_recommended_movies.append(movie)
+    return user_recommended_movies
 
+
+def get_rec_from_favorites(user_data):
+    # use helper function to get user unique movies
+    user_unique_movies = get_unique_watched(user_data)
+    # create list with favorite movies
+    favorite_movies = []
+    for movie in user_data["favorites"]:
+        favorite_movies.append(movie)
+    # iterate over favorite movies and check if has not been watched by friends
+    movie_from_favorites = []
+    for movie in favorite_movies:
+        if movie in user_unique_movies:
+            movie_from_favorites.append(movie)
+    return movie_from_favorites
