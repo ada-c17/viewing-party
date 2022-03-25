4377079ade9ff4838068e6bb8febfe5efec46ae7
mit <mit.magh@gmail.com>
2022-03-24 22:39:16 -0700

    wave 3 assertions added

--- play_tester.py
+++ play_tester.py
@@ -9,13 +9,13 @@ import pprint
 pp = pprint.PrettyPrinter(indent=4)
 
 # play testing section
-print("\n-----Wave 01 test data-----")
-pp.pprint(HORROR_1)
-pp.pprint(FANTASY_1)
-pp.pprint(FANTASY_2)
+# print("\n-----Wave 01 test data-----")
+# pp.pprint(HORROR_1)
+# pp.pprint(FANTASY_1)
+# pp.pprint(FANTASY_2)
 
-print("\n-----Wave 02 user_data-----")
-pp.pprint(clean_wave_2_data())
+# print("\n-----Wave 02 user_data-----")
+# pp.pprint(clean_wave_2_data())
 
 print("\n-----Wave 03 user_data-----")
 pp.pprint(clean_wave_3_data())
