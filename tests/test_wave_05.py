0269ebdae9a1b5b7c971a0b8207ac9a744ea853f
Jamie H <jamiewhorvath@gmail.com>
2022-03-24 22:27:41 -0400

    Completed missing asserts in wave 1,3,5

--- tests/test_wave_05.py
+++ tests/test_wave_05.py
@@ -53,9 +53,11 @@ def test_new_genre_rec_from_empty_friends():
         ]
     }
 
-    # *********************************************************************
-    # ****** Complete the Act and Assert Portions of theis tests **********
-    # *********************************************************************
+    # Act
+    recomendations = get_new_rec_by_genre(sonyas_data)
+
+    # Assert
+    assert len(recomendations) == 0
 
 #@pytest.mark.skip()
 def test_unique_rec_from_favorites():
