7fd2b151a1aa5ccd88e6e8f397a698fb0f33ee84
Ying G <yingg@yings-air.lan>
2022-04-01 16:45:02 -0700

    complete tests and refactor based on suggestions

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -116,12 +116,11 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     updated_data = watch_movie(janes_data, MOVIE_TITLE_1)
 
     # Assert
-    assert len(updated_data["watchlist"]) is 0
     assert len(updated_data["watched"]) is 1
-    
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    assert len(updated_data["watchlist"]) is 0
+    assert updated_data["watched"] == [{"title": MOVIE_TITLE_1,
+                                        "genre": GENRE_1,
+                                        "rating": RATING_1}]
 
 # @pytest.mark.skip()
 def test_moves_movie_from_watchlist_to_watched():
@@ -141,10 +140,8 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) is 1
     assert len(updated_data["watched"]) is 2
-    
-    # *******************************************************************************************
-    # ****** Add assertions here to test that the correct movie was added to "watched" **********
-    # *******************************************************************************************
+    assert updated_data["watched"] == [FANTASY_2,HORROR_1]
+    assert HORROR_1 not in updated_data["watchlist"]
 
 # @pytest.mark.skip()
 def test_does_nothing_if_movie_not_in_watchlist():
--- viewing_party/party.py
+++ viewing_party/party.py
@@ -1,5 +1,5 @@
 def create_movie(title,genre,rating):
-    if not bool(title) or not bool(genre) or not bool(rating):
+    if not title or not genre or not rating:
         return None
     return {"title":title,"genre":genre,"rating":rating}
 
@@ -12,11 +12,14 @@ def add_to_watchlist(user_data,movie):
     return user_data
 
 def watch_movie(user_data,title):
-    for movie in user_data["watchlist"]:
+    index_to_move = None # initiate flag index_to_move to store index of movie in watchlist if movie is found to later append it to watched and remove it from watchlist
+    for index, movie in enumerate(user_data["watchlist"]):
         if movie["title"] == title:
-            user_data["watchlist"].remove(movie)
-            user_data["watched"].append(movie)
-            return user_data
+            index_to_move = index
+            break # break here to save the for loop redundant additional iterations
+    if index_to_move is not None: # cannot use if not index_to_move as 0 is a valid index number but evaluated as a falsey
+        user_data["watched"].append(user_data["watchlist"][index_to_move])
+        del user_data["watchlist"][index_to_move]   
     return user_data
 
 def get_watched_avg_rating(user_data):
@@ -29,18 +32,18 @@ def get_watched_avg_rating(user_data):
     return ratings_sum/n
         
 def get_most_watched_genre(user_data):
-    genre_counts = {}
     if len(user_data["watched"]) == 0:
         return None
+    genre_counts = {}
+    max_count = 0 # initiate max_count variable to 0 to keep track of max occurrences we've seen with each iteration of the for loop
+    max_genre = None # initiate max_genre variable to None to keep track of which genre we've seen has the max occurrences with each iteration of the for loop
     for movie in user_data["watched"]:
         genre = movie["genre"]
-        if genre in genre_counts:
-            genre_counts[movie["genre"]]+=1
-        else:
-            genre_counts[movie["genre"]] = 1
-    
-    key = max(genre_counts,key=lambda k: genre_counts[k])
-    return key
+        genre_counts[genre] = genre_counts.get(genre,0)+1 # create genre_counts[genre] and set to 1 if doesn't exist, add 1 if it does for each time we see a movie with the genre
+        if genre_counts[genre] > max_count:
+            max_count = genre_counts[genre]
+            max_genre = genre
+    return max_genre
 
 def get_unique_watched(user_data):
     friends_list = user_data["friends"]
@@ -67,13 +70,11 @@ def get_friends_unique_watched(user_data):
     return friends_watched_unique
 
 def get_available_recs(user_data):
+    friends_watched_unique = get_friends_unique_watched(user_data) # use get_friends_unique_watched first so later we only have to check if each movie's host is one in user's subscriptions
     recommended = []
-    friends_list = user_data["friends"]
-    watched_list = [movie["title"] for movie in user_data["watched"]]
-    for friend in friends_list:
-        for movie in friend["watched"]:
-            if movie not in recommended and movie["title"] not in watched_list and movie["host"] in user_data["subscriptions"]:
-                recommended.append(movie)
+    for movie in friends_watched_unique:
+        if movie["host"] in user_data["subscriptions"]:
+            recommended.append(movie)
     return recommended
 
 def get_new_rec_by_genre(user_data):
