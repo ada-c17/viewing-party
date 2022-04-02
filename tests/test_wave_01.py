b5aa9131da9b70948f3888a7022ac6fb7945ea77
Joanna Dudley <jfdudleybmc@gmail.com>
2022-04-02 16:35:29 -0400

    refactored test_wave_01 in accordance with instructor comments

--- tests/test_wave_01.py
+++ tests/test_wave_01.py
@@ -1,3 +1,4 @@
+from turtle import update
 import pytest
 # NOTE: In production code, we developers should change import * to something more specific. 
 # Due to some constraints of this project, we will import * in our test files.
@@ -119,7 +120,14 @@ def test_moves_movie_from_watchlist_to_empty_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 0
     assert len(updated_data["watched"]) is 1
-    assert updated_data["watched"][0]["title"] == MOVIE_TITLE_1
+    
+    # refactoring post-submission - recommended to check all keys of dictionary
+    # OG CODE: assert updated_data["watched"][0]["title"] == MOVIE_TITLE_1
+    watched_movie = updated_data["watched"][0]
+    assert watched_movie["title"] == MOVIE_TITLE_1
+    assert watched_movie["rating"] == RATING_1
+    assert watched_movie["genre"] == GENRE_1
+
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
     # *******************************************************************************************
@@ -142,7 +150,10 @@ def test_moves_movie_from_watchlist_to_watched():
     # Assert
     assert len(updated_data["watchlist"]) == 1
     assert len(updated_data["watched"]) == 2
-    assert updated_data["watched"] == [FANTASY_2, HORROR_1]
+    # refactoring post-submission - recommended to check all keys of dictionary
+    # OG CODE: assert updated_data["watched"] == [FANTASY_2, HORROR_1]
+    assert FANTASY_2 in updated_data["watched"]
+    assert HORROR_1 in updated_data["watched"]
     
     # *******************************************************************************************
     # ****** Add assertions here to test that the correct movie was added to "watched" **********
